bdc48753259e021b29a08d35dad4d132a23a3dcc
Aaron Swartz <me@aaronsw.com>
2012-04-19 11:50:44 -0400

    import rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Victorykit::Application.config.secret_token = '11aa30cbaeb3565470e5d6ad7da12d07636154e7b75fcd53bb265be50a71e3ce14aaade321a9e1b7fcec7b619b6dfd293bc1774c0a0c730b4fc017cda66d1633'
