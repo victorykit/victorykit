65a58bd329a5e76e4fa55e10f0a69c4c42215f18
Jennifer Smith <jensmith@thoughtworks.com>
2012-04-23 15:42:23 -0400

    WIP - adding a signature

--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe SignaturesController do
-
+  context "signing a petition" do
+    it "should let me sign a petition" do
+      petition = Petition.create!
+      post :create, :petition => petition, :signature => {:name => "Bob", :email => "bob@thoughtworks.com"}
+      
+    end
+  end
 end
