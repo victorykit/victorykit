bdba03f148cc17b1194098534e97742323377c7f
Jim Arnold <jim@thoughtworks.com>
2012-04-23 16:08:59 -0400

    now adding signatures to petitions

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,2 +1,8 @@
 class SignaturesController < ApplicationController
+  def create
+    petition = Petition.find params[:petition_id]
+    petition.signatures.push(Signature.new(params[:signature]))
+    petition.save
+    render :text => "signed!"
+  end
 end
--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -4,8 +4,8 @@ describe SignaturesController do
   context "signing a petition" do
     it "should let me sign a petition" do
       petition = Petition.create!
-      post :create, :petition => petition, :signature => {:name => "Bob", :email => "bob@thoughtworks.com"}
-      
+      post :create, :petition_id => petition.id, :signature => {:name => "Bob", :email => "bob@thoughtworks.com"}
+      petition.signatures[0].name.should eq "Bob"
     end
   end
 end
