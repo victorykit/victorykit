cbd56c29ccf87993adac5d2b3289f3e44fbe43d3
Matt Urbanski <matt@iflowfor8hours.info>
2012-04-23 17:18:22 -0400

    add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
+  end
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates_uniqueness_of :email
+  attr_accessible :email, :password, :password_confirmation
+end
