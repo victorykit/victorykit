6b84952220913a95cbbab508bfdd9f9d24a7cfa1
Jim Arnold <jim@thoughtworks.com>
2012-04-23 16:29:13 -0400

    added test for signature email value

--- spec/controllers/signatures_controller_spec.rb
+++ spec/controllers/signatures_controller_spec.rb
@@ -4,8 +4,9 @@ describe SignaturesController do
   context "signing a petition" do
     it "should let me sign a petition" do
       petition = Petition.create!
-      post :create, :petition_id => petition.id, :signature => {:name => "Bob", :email => "bob@thoughtworks.com"}
+      post :create, :petition_id => petition.id, :signature => {:name => "Bob", :email => "bob@my.com"}
       petition.signatures[0].name.should eq "Bob"
+      petition.signatures[0].email.should eq "bob@my.com"
     end
   end
 end
