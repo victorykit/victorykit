ffb0c4777f1c0d27988792bebf72099fddef170b
Michelle/Jen <michd2005@gmail.com>
2012-04-24 09:58:36 -0400

    Added non-authenticating login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    session[:user_id] = User.find_by_email(params[:email]).id
+    redirect_to root_path
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,6 +7,16 @@ describe SessionsController do
       get 'new'
       response.should be_success
     end
+    context "user logs in with valid credentials" do
+      before(:each) do 
+         @user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+         post :create, email: "bob@here.com", password:"opensesame"
+      end
+      it "adds the user id to the session" do
+        session[:user_id].should == @user.id
+      end
+      it { should redirect_to root_path }
+    end
   end
 
 end
