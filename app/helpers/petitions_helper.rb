9cb93e1302bb6c19479bb32989301201d47232df
mandersen <manderdev@gmail.com>
2012-04-24 17:16:56 -0400

    fiddling with image url for fb meta info

--- app/helpers/petitions_helper.rb
+++ app/helpers/petitions_helper.rb
@@ -1,10 +1,11 @@
 module PetitionsHelper
   def petition_to_open_graph(petition)
-    { 'og:title' => petition.title, 
+    { 
+      'og:title' => petition.title, 
       'og:type' => 'cause', 
       'og:description' => petition.description,
       'og:url' => petition_url(petition),
-      'og:image' => 'act.watchdog.net' + image_path('petition-fb.png'),
+      'og:image' => URI.join(root_url, image_path('petition-fb.png')),
       'og:site_name' => 'Victory Kit'
     }
   end
