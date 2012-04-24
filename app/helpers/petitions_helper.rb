930249ded3429d6b207a80ae43a6632b78bb067e
mandersen <manderdev@gmail.com>
2012-04-24 16:45:23 -0400

    hardcoding image path to see if it then shows up in fb recommend

--- app/helpers/petitions_helper.rb
+++ app/helpers/petitions_helper.rb
@@ -4,7 +4,7 @@ module PetitionsHelper
       'og:type' => 'cause', 
       'og:description' => petition.description,
       'og:url' => petition_url(petition),
-      'og:image' => image_path('petition-fb.png'),
+      'og:image' => 'act.watchdog.net' + image_path('petition-fb.png'),
       'og:site_name' => 'Victory Kit'
     }
   end
