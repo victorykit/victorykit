baf764b4287ab663884eb57dc21e0e80714fd359
Michelle D'Souza <michd2005@gmail.com>
2012-04-24 19:11:21 -0400

    Added show, edit, update functionality to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,45 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
+  
+  def update
+    @user = User.find(params[:id])
 
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   def create
     @user = User.new(params[:user])
     @user.is_super_user = false
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,73 @@
 require 'spec_helper'
 
 describe UsersController do
+  
+  describe "GET index" do
+    it "shows all users" do
+      user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+      get :index
+      assigns(:users).should eq([user])
+    end
+  end
+  
+  describe "GET show" do
+    it "shows the requested user" do
+      user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+      get :show, {:id => user.to_param}
+      assigns(:user).should eq(user)
+    end
+  end
+  
+  describe "GET new" do
+    it "assigns a new user" do
+      get :new
+      assigns(:user).should be_a_new(User)
+    end
+  end
+  
+  describe "GET edit" do
+    it "assigns the requested user" do
+      user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+      get :edit, {:id => user.to_param}
+      assigns(:user).should eq(user)
+    end
+  end
+  
+  describe "PUT update" do
+    describe "with valid params" do
+      it "updates the requested user" do
+        user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+        User.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, {:id => user.to_param, :user => {'these' => 'params'}}
+      end
 
+      it "assigns the requested user as @user" do
+        user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+        put :update, {:id => user.to_param}
+        assigns(:user).should eq(user)
+      end
+
+      it "redirects to the user" do
+        user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+        put :update, {:id => user.to_param}
+        response.should redirect_to(user)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the user as @user" do
+        user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+        User.any_instance.stub(:save).and_return(false)
+        put :update, {:id => user.to_param, :user => {}}
+        assigns(:user).should eq(user)
+      end
+
+      it "re-renders the 'edit' template" do
+        user = User.create!(password:"opensesame", password_confirmation: "opensesame", email: "bob@here.com")
+        User.any_instance.stub(:save).and_return(false)
+        put :update, {:id => user.to_param, :user => {}}
+        response.should render_template("edit")
+      end
+    end
+  end
 end
