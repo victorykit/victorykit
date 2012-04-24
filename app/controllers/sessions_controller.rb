b22c03c0792b12c1a3e3954671d4f8332fbc87fc
Michelle D'Souza <michd2005@gmail.com>
2012-04-24 14:44:45 -0400

    Refactored user authentication to use one less if statement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,16 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if(!user.nil?)
-      if user.authenticate(params[:password])
-        session[:user_id] = User.find_by_email(params[:email]).id
-        redirect_to root_path
-      else
-        flash.now[:error] = "Invalid username or password"
-        render :new
-      end
+    if user && user.authenticate(params[:password])
+      session[:user_id] = User.find_by_email(params[:email]).id
+      redirect_to root_path
     else
       flash.now[:error] = "Invalid username or password"
       render :new
     end
+    #else
+    #  flash.now[:error] = "Invalid username or password"
+    #  render :new
+    #end
   end
 end
