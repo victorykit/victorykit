95409b44a6c3357849ceed07d2979108d5dd1771
Michelle D'Souza <michd2005@gmail.com>
2012-04-24 14:38:36 -0400

    Removed the use of signature as an instance

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,10 +1,10 @@
 class SignaturesController < ApplicationController
   def create
     petition = Petition.find params[:petition_id]
-    @signature = Signature.new(params[:signature])
-    @signature.ip_address = request.remote_ip
-    @signature.user_agent = request.env["HTTP_USER_AGENT"]
-    petition.signatures.push(@signature)
+    signature = Signature.new(params[:signature])
+    signature.ip_address = request.remote_ip
+    signature.user_agent = request.env["HTTP_USER_AGENT"]
+    petition.signatures.push(signature)
     petition.save
     render
   end
