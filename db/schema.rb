5adcbcdff5bf791522e571425d0fd1c11e214ed9
Michelle D'Souza <michd2005@gmail.com>
2012-04-25 14:40:53 -0400

    Schema change from previous db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(:version => 20120425182101) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.integer  "owner_id",    :null => false
+    t.integer  "owner_id"
   end
 
   create_table "signatures", :force => true do |t|
