3e9c80b29811f5939215f437c0bad49b34d6bd2c
Michelle D'Souza <michd2005@gmail.com>
2012-04-25 10:30:08 -0400

    Customizing user edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :email
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :is_super_user
 end
