b94792d174c717f1895430e39b1263ad9b36a3e1
Aaron Swartz <me@aaronsw.com>
2012-04-26 15:58:53 -0400

    resolve merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,10 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @users }
-    end
   end
   
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @user }
-    end
   end
   
   def edit
@@ -30,20 +20,16 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update_attributes(params[:user])
+        redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render action: "edit"
     end
   end
   
   def create
     @user = User.new(params[:user])
+    #@@ can we set these as default on the model?
     @user.is_super_user = false
     @user.is_admin = false
     if @user.save
