920345475057dcfbe9522f0c513ac47e2ca57a09
Jim Arnold <jim@thoughtworks.com>
2012-04-26 14:25:09 -0400

    added missing javascript files

---
+++ app/assets/javascripts/petition.js
@@ -0,0 +1,8 @@
+jQuery(function(){
+	var cookie = $.cookie('signed_petitions') || '';	
+	var petitionIds = cookie.split("|");
+	var currentPetitionId = $('#petitionId').val();
+	if($.inArray(currentPetitionId, petitionIds) > -1) {
+		$('#thanks-for-signing-message').show();
+	}
+});
