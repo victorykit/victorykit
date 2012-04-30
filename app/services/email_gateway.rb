6f1b63e5ffd2c291d84356dfb0554b7653d5e7a4
Jennifer Smith <jensmith@thoughtworks.com>
2012-04-30 15:28:58 -0400

    using env vars for AWS keys

--- app/services/email_gateway.rb
+++ app/services/email_gateway.rb
@@ -2,14 +2,14 @@ require 'aws/ses'
 
 class EmailGateway
   def self.send_email email
-    raise "boom!"
-    #ses = AWS::SES::Base.new(:access_key_id => 'AKIAJKCNQLB33WKHBW5Q', :secret_access_key => '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')
     
-    #ses.send_email :to      => ['jensmith@thoughtworks.com'],
-    #             :source    => 'jensmith@thoughtworks.com',
-    #             :subject   => 'A Victory for Spam!',
-    #             :text_body => 'I hope Amazon doesnt you charge for this'
+    ses = AWS::SES::Base.new(:access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"])
+    
+    ses.send_email :to      => ['jensmith@thoughtworks.com'],
+                 :source    => 'jensmith@thoughtworks.com',
+                 :subject   => 'A Victory for Spam!',
+                 :text_body => 'I hope Amazon doesnt you charge for this'
   
-    #sleep 1000 - don't charge Jen's account!
+    sleep 1 #- don't charge Jen's account!
   end
 end
