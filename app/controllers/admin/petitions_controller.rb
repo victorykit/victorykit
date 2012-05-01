9958230e65bafcba26a766606b964668b7a17c6b
mandersen <manderdev@gmail.com>
2012-05-01 16:12:53 -0400

    introduced Analytics class to admin petitions controller; added created_member flag to signature

--- app/controllers/admin/petitions_controller.rb
+++ app/controllers/admin/petitions_controller.rb
@@ -1,7 +1,29 @@
 class Admin::PetitionsController < ApplicationController
-
   def index
     @petitions = Petition.all
+    @analytics = Analytics.new(AnalyticsGateway.get_report_results)
+  end
+
+  class Analytics
+    include Rails.application.routes.url_helpers
+
+    def initialize(data)
+      @data = data
+    end
+
+    def [](petition)
+      hits = @data.find { |k,v| k == petition_path(petition) }[1].pageviews.to_i    
+      signatures = petition.signatures.count
+      new_members = petition.signatures.count(conditions: "created_member is true")
+      
+      {
+        :hits => hits,
+        :signatures => signatures,
+        :conversion => (signatures.to_f / hits.to_f),
+        :new_members => new_members,
+        :virality => (new_members.to_f / signatures.to_f)
+      }
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120430153714) do
+ActiveRecord::Schema.define(:version => 20120501195241) do
 
   create_table "petitions", :force => true do |t|
     t.text     "title",       :null => false
@@ -22,14 +22,15 @@ ActiveRecord::Schema.define(:version => 20120430153714) do
   end
 
   create_table "signatures", :force => true do |t|
-    t.string   "name",         :null => false
-    t.string   "email",        :null => false
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.integer  "petition_id",  :null => false
-    t.string   "ip_address",   :null => false
-    t.string   "user_agent",   :null => false
+    t.string   "name",           :null => false
+    t.string   "email",          :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.integer  "petition_id",    :null => false
+    t.string   "ip_address",     :null => false
+    t.string   "user_agent",     :null => false
     t.string   "browser_name"
+    t.boolean  "created_member"
   end
 
   create_table "users", :force => true do |t|
