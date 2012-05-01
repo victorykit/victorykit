7400288d9813963367422164b3c1ad284a900411
rachel kammerer <rlkammerer@gmail.com>
2012-05-01 14:37:58 -0400

    rachel/jen - fixed broken specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,9 +4,9 @@ describe SessionsController do
 
   describe "POST 'create'" do
     context "user logs in with valid credentials" do
-      before(:each) do 
+      before(:each) do
          @user = create(:user, password:"opensesame", email: "bob@here.com")
-         post :create, email: "bob@here.com", password:"opensesame"
+         post :create, new_session: { email: "bob@here.com", password:"opensesame"}
       end
       it "adds the user id to the session" do
         session[:user_id].should == @user.id
@@ -14,9 +14,9 @@ describe SessionsController do
       it { should redirect_to root_path }
     end
     context "user attempts to login with wrong password" do
-      before(:each) do 
+      before(:each) do
          @user = create(:user, email: "bob@here.com", password: "supersecret")
-         post :create, email: "bob@here.com", password:"closesesame"
+         post :create, new_session: {email: "bob@here.com", password:"closesesame"}
       end
       it "renders the login page" do
         response.should render_template :new
@@ -25,13 +25,13 @@ describe SessionsController do
           session[:user_id].should be_nil
       end
       it "displays message to user" do
-          flash.now[:error].should == "Invalid username or password" 
+          flash.now[:error].should == "Invalid username or password"
       end
     end
     context "user attempts to login with wrong email" do
-      before(:each) do 
+      before(:each) do
         @user = create(:user, email:"bob@ajob.com", password: "opensesame")
-         post :create, email: "jim@here.com", password:"opensesame"
+         post :create, new_session: {email: "jim@here.com", password:"opensesame"}
       end
       it "renders the login page" do
         response.should render_template :new
@@ -40,7 +40,7 @@ describe SessionsController do
           session[:user_id].should be_nil
       end
       it "displays message to user" do
-          flash.now[:error].should == "Invalid username or password" 
+          flash.now[:error].should == "Invalid username or password"
       end
     end
   end
@@ -57,7 +57,7 @@ describe SessionsController do
       end
       it { should redirect_to root_path }
       it "displays message logged out message to user" do
-          flash.now[:notice].should == "Logged out!" 
+          flash.now[:notice].should == "Logged out!"
       end
     end
   end
