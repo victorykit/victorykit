58092907b903521f90227191112721bc33d28b17
Jim Arnold <jim@thoughtworks.com>
2012-05-01 11:14:36 -0400

    fixed prod config; removed unnecessary staging config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Victorykit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.redis = {:url => ENV["REDISTOGO_URL"]}
+  config.redis = {:uri => ENV["REDISTOGO_URL"]}
 end
