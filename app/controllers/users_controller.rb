8609605effbd0a99b077fd3c7c5cde2a215fa02c
Michelle D'Souza <michd2005@gmail.com>
2012-05-01 17:45:42 -0400

    IsAdmin and IsSuperUser now get set by default when a new user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,6 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
-    #@@ can we set these as default on the model?
-    @user.is_super_user = false
-    @user.is_admin = false
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
