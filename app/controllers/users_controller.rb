2162daa8d3a3e1f0620336de8dae07c89825fc2c
Michelle D'Souza <michd2005@gmail.com>
2012-05-02 11:04:59 -0400

    Added validation and error messages for update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,16 +16,21 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    if @user && @user.authenticate(params[:user][:current_password]) && (params[:user][:new_password] == params[:user][:verify_password])
-      if @user.update_attributes(:password => params[:user][:new_password])
-        redirect_to edit_user_url, notice: 'Password was successfully updated.'
+    if @user && @user.authenticate(params[:user][:current_password]) 
+      if params[:user][:new_password].empty? || params[:user][:verify_password].empty? 
+        flash.now[:error] = "Please enter a new password and verify password"
+        render action: "edit"
+      elsif params[:user][:new_password] == params[:user][:verify_password]
+        @user.update_attributes(:password => params[:user][:new_password])
+        redirect_to edit_user_url, notice: "Password was successfully updated"
       else
+        flash.now[:error] = "New password does not match the verified password"
         render action: "edit"
       end
     else
+      flash.now[:error] = "Current password is incorrect"
       render action: "edit"
     end
   end
   
 end
-
