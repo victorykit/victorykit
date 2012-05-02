3046d0be958d957006080bc67bd19b32025a24bc
Aaron Swartz <me@aaronsw.com>
2012-05-02 17:48:08 -0400

    don't return 404 if invalid user ID

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
     
