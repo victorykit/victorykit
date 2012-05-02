7ecd2519f68bdbc4b3253bb773710d500e6e67b2
Jennifer Smith <jensmith@thoughtworks.com>
2012-05-02 16:31:44 -0400

    Added the ses initialization to the Settings file

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
-  :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+  :access_key_id     => Settings.aws.access_key_id,
+  :secret_access_key => Settings.aws.secret_access_key
