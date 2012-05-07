6b26f43d0dfd95205429aaccb2f9a5a007fcce6c
Jim Arnold <jim@thoughtworks.com>
2012-05-07 15:12:46 -0400

    trying redis sessions again..

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Victorykit::Application.config.session_store :cookie_store, key: '_victorykit_session'
+$redis_uri = Settings.redis.uri
+$VERBOSE=true
+Victorykit::Application.config.session_store :redis_store, :servers => Settings.redis.uri
+#Victorykit::Application.config.session_store :cookie_store, key: '_victorykit_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
