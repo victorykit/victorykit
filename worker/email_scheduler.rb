7564fd2ae549f1697e3cf5b78f3c80d4c69d5bb1
Michelle D'Souza <michd2005@gmail.com>
2012-05-08 11:16:03 -0600

    Added lock to mailerprocesstracker row to avoid race condition

--- worker/email_scheduler.rb
+++ worker/email_scheduler.rb
@@ -6,11 +6,9 @@ class EmailScheduler
   
   def self.schedule_email
     max_emails_per_day = 10000
-    mailer_process = MailerProcessTracker.find_by_id(1)
+    
     while 1
-      #Lock table
-      #x = MailerProcessTracker.connection.execute("SELECT * FROM mailer_process_trackers WHERE mailer_process_trackers.id = 1").to_a 
-      #ActiveRecord::Base.connection.execute("LOCK TABLES users WRITE")
+      mailer_process = MailerProcessTracker.find_by_id(1, :lock => true)
       if !mailer_process.nil? && !mailer_process.is_locked?
         begin
           update_mailer_process(mailer_process, true)
