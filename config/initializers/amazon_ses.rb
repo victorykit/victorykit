4815a034baca9dfbdf96f9227d232fa7ded63d3d
Aaron Swartz <me@aaronsw.com>
2012-05-09 14:47:18 -0400

    correct key name

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => Settings.aws.access_key_id,
   :secret_access_key => Settings.aws.secret_access_key
 
-if ENV['DKIM_SECRET_KEY']
+if ENV['DKIM_PRIVATE_KEY']
   Dkim::domain      = 'watchdog.net'
   Dkim::selector    = 'mail'
-  Dkim::private_key = ENV['DKIM_SECRET_KEY']
+  Dkim::private_key = ENV['DKIM_PRIVATE_KEY']
 
   Dkim::signable_headers = Dkim::DefaultHeaders - \
       %w{Message-ID Resent-Message-ID Date Return-Path Bounces-To}
