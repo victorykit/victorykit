fbb4d5b53f8c848a0ef5930380d185e909c47cd1
Michelle D'Souza <michd2005@gmail.com>
2012-05-09 09:53:14 -0600

    Removed commented out code

--- app/mailers/scheduled_email.rb
+++ app/mailers/scheduled_email.rb
@@ -9,7 +9,6 @@ class ScheduledEmail < ActionMailer::Base
   def new_petition(petition, email, sent_email_id)
     @petition_link = petition_url(petition) + "?n=" + Hasher.generate(sent_email_id)
     @unsubscribe_link = new_unsubscribe_url(Unsubscribe.new)
-    #+ "?n=" + Hasher.generate(sent_email_id)
     @petition = petition
     
     mail(subject: "New Petition: '#{petition.title}'!", to: email).deliver
