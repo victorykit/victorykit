c2a768938d5458a296503e33603586f9c9223ee1
Aaron Swartz <me@aaronsw.com>
2012-05-09 14:18:44 -0400

    turn on DKIM signing

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,14 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => Settings.aws.access_key_id,
   :secret_access_key => Settings.aws.secret_access_key
+
+if ENV['DKIM_SECRET_KEY']
+  Dkim::domain      = 'watchdog.net'
+  Dkim::selector    = 'mail'
+  Dkim::private_key = ENV['DKIM_SECRET_KEY']
+
+  Dkim::signable_headers = Dkim::DefaultHeaders - \
+      %w{Message-ID Resent-Message-ID Date Return-Path Bounces-To}
+
+  ActionMailer::Base.register_interceptor('Dkim::Interceptor')
+end
