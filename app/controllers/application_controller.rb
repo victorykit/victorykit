503e77a19a16cc0e51c4f7d3a2272bad0bb63d1d
Aaron Swartz <me@aaronsw.com>
2012-05-10 14:13:26 -0400

    move 403 into a template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_403
-    render :text => "You are not authorized to view this page", :status => 403
+    render :file => "#{Rails.root}/public/403.html", :status => 403
   end
   
   def can(permission)
