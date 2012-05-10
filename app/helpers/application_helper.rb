9f0a8c94cc23500ca4ae033f163bda0cd2140325
rachel kammerer <rlkammerer@gmail.com>
2012-05-10 12:33:40 -0400

    make facebook like more focal after signing a petition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,11 +15,16 @@ module ApplicationHelper
   end
 
   def facebook_like(url)
-    content_tag :iframe, nil, :src => 
-      "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&layout=standard&action=recommend&show_faces=true&width=450&height=80&font=arial&colorscheme=light", 
+    content_tag :iframe, nil, :src =>
+      "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&layout=standard&action=like&show_faces=true&width=150&height=80&font=arial&colorscheme=light",
       :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
   end
-  
+
+  def facebook_like_small(url)
+    content_tag :iframe, nil, :src =>
+      "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&layout=button_count&action=recommend&show_faces=true&width=100&height=80&font=arial&colorscheme=light",
+      :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
   def google_analytics_tracker
     analytics_id = Settings.google_analytics.analytics_id
     javascript_tag "var _gaq = _gaq || [];
@@ -32,11 +37,11 @@ module ApplicationHelper
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
       })();"
   end
-  
+
   def float_to_percentage(f)
     number_to_percentage(f*100, precision: 2)
   end
-  
+
   def format_date_time(d)
     d.strftime("%Y-%m-%d %H:%M")
   end
