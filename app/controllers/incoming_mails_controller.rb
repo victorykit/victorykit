cee0c5f3047ca70b2c0a9fca4599bf97ad8720e7
Michelle D'Souza <michd2005@gmail.com>
2012-05-10 16:51:48 -0600

    Still need to add the meat to the incoming mails controller

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -3,16 +3,15 @@ class IncomingMailsController < ApplicationController
     skip_before_filter :verify_authenticity_token
 
     def create
+=begin
       message = Mail.new(params[:message])
-      #Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
-      #Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
-      if (params[:envelope][:to].to_s.start_with? 'bounce-')
+      Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
+      Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+      to_email = params[:envelope][:to]
+      if (to_email && to_email.to_s.start_with? 'bounce-')
         BounceReceiver.receive(message.to_s)
       end
       render :text => 'success', :status => 200 # a status of 404 would reject the mail
-    end
-    
-    def new
-      message = Mail.new(params[:message])
+=end
     end
 end
