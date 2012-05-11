cc40662024bd58bcb023d5822a70a56693f12b1d
Jim Arnold <jim@thoughtworks.com>
2012-05-11 11:48:25 -0400

    added missing smoke tests; fixed rake task for CI

--- lib/tasks/smoke_tests.rake
+++ lib/tasks/smoke_tests.rake
@@ -1,3 +1,5 @@
+require 'rspec/core/rake_task'
+
 namespace :spec do
   desc "Run the smoke tests"
   RSpec::Core::RakeTask.new(:smoke) do |t|
---
+++ spec/smoke/front_page.rb
@@ -0,0 +1,19 @@
+require 'smoke_spec_helper.rb'
+
+describe "front page" do
+  before :each do
+    $driver.navigate.to HOST_URL
+  end
+  
+  it "should not blow up" do
+    welcome = $driver.find_element(:class =>'title')
+    welcome.text.should == 'Start a new petition'
+  end
+  
+  it "should ask users to log in before creating a petition" do
+    link = $driver.find_element(:class => 'btn-primary')
+    link.click
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    wait.until { $driver.find_element(:class => "email") }
+  end
+end
