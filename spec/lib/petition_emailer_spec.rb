a6b61c6c541a714930b6228ce7f50a3163e9003c
Jim Arnold <jim@thoughtworks.com>
2012-05-15 15:01:31 -0400

    added tests for petition_emailer

---
+++ spec/lib/petition_emailer_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'petition_emailer'
+
+describe PetitionEmailer do
+  let(:petition){create(:petition, :to_send => true)}
+  let(:member){create(:member)}
+  it "asks for a random member" do
+    Member.should_receive(:random_and_not_recently_contacted).and_return(member)
+    PetitionEmailer.send
+  end
+  it "aborts if no member is found" do
+    Member.should_receive(:random_and_not_recently_contacted).and_return(nil)
+    ScheduledEmail.should_not_receive(:new_petition)
+    PetitionEmailer.send
+  end
+  it "spins to find a petition" do
+    PetitionEmailer.should_receive(:spin!).with("email_scheduler", :signups_off_email, [petition.id.to_s], {session_id: member.id}).and_return(petition.id.to_s)
+    PetitionEmailer.send
+  end
+  it "aborts if no petitions found" do
+    PetitionEmailer.stub(:spin!).and_return nil
+    ScheduledEmail.should_not_receive(:new_petition)
+    PetitionEmailer.send
+  end
+  it "sends the petition to the member" do
+    ScheduledEmail.should_receive(:new_petition).with(petition, member)
+    PetitionEmailer.send
+  end
+end
--- spec/support/stubs/whiplash.rb
+++ spec/support/stubs/whiplash.rb
@@ -1,7 +1,7 @@
 module Bandit
   def win! *args
   end
-  def spin! test_name, goals, options
+  def spin! test_name, goals, options, my_session=nil
     options.first
   end
   def dummy!
