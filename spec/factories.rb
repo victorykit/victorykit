566af11a180d023680d3cf61cd954d0d29d3ae0e
Jim Arnold <jim@thoughtworks.com>
2012-05-16 10:25:23 -0400

    fixed factorygirl warnings

--- spec/factories.rb
+++ spec/factories.rb
@@ -18,12 +18,12 @@ FactoryGirl.define do
       ignore do
         signature_count 10
       end
-      after_create do |petition, evaluator|
+      after(:create) do |petition, evaluator|
         FactoryGirl.create_list(:signature, evaluator.signature_count, petition: petition)
       end
     end 
     factory :petition_with_one_signature_per_day_since_last_month do
-      after_create do |petition, evaluator|
+      after(:create) do |petition, evaluator|
         today = Date.today
         last_month = today << 1
         
