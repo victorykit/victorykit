fbbd8f72bcce9c9dcd79e7f04e78aedcfb5cc495
Aaron Swartz <me@aaronsw.com>
2012-05-16 10:04:01 -0400

    bias in favor of giving things more of a chance also clean up some style stuff

--- lib/whiplash.rb
+++ lib/whiplash.rb
@@ -1,6 +1,8 @@
 require 'distribution'
 require 'redis'
 
+FAIRNESS_CONSTANT = 3
+
 class Float
   def to_1if0
     self.zero? ? 1 : self
@@ -14,21 +16,22 @@ module Bandit
     stddev += ((observations - victories) * ((0-mean))**2)
     stddev = Math.sqrt(stddev * 1.0/((observations.to_f-1).to_1if0))
     stddev = stddev.to_1if0/Math.sqrt(observations.to_f.to_1if0)
-    Distribution::Normal.rng(mean, stddev).call
+    out = [0, Distribution::Normal.rng(mean, stddev).call].max
+    return out + (FAIRNESS_CONSTANT * (1.0/observations.to_f.to_1if0))
   end
 
   def best_guess(options)
     guesses = {}
     options.each { |o, v| guesses[o] = arm_guess(v[0], v[1]) }
     best = options.keys.select { |o| guesses[o] == guesses.values.max }
-    best.sample
+    return best.sample
   end
 
   def redis_nonce(mysession)
     # force creation of a session_id
     mysession[:tmp] = 1
     mysession.delete(:tmp)
-    "#{mysession[:session_id]}_#{Random.rand}"
+    return "#{mysession[:session_id]}_#{Random.rand}"
   end  
 
   def spin!(test_name, goal, options=[true, false], mysession=nil)
@@ -46,7 +49,7 @@ module Bandit
     choice = best_guess(loptions)
     REDIS.zadd("whiplash/#{test_name}/#{choice}/spin", Time.now.to_f, redis_nonce(mysession))
     mysession[test_name] = choice
-    choice
+    return choice
   end
 
   def win_on_option!(test_name, choice, mysession=nil)
