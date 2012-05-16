14bd5a1bc0561a97fa4ebb263f98cef41598d59a
Michelle D'Souza <michd2005@gmail.com>
2012-05-16 16:13:15 -0600

    Added connection to local db for testing

--- app/services/demand_progress_gateway.rb
+++ app/services/demand_progress_gateway.rb
@@ -1,7 +1,7 @@
 class DemandProgressGateway < ActiveRecord::Base
   self.abstract_class = true
   
-  establish_connection(Settings.demand_progress.db_uri)
+  establish_connection :development
 end
 
 class UnsubscribeRequest
