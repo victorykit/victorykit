25a9d7dab6a396539af3c90dc4fb536349a09438
Michelle D'Souza <michd2005@gmail.com>
2012-05-16 13:32:09 -0600

    Added rails logging to incoming mails controller

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -8,9 +8,11 @@ class IncomingMailsController < ApplicationController
       message = Mail.new(params[:message])
       to_address = params[:to]
       if (to_address and to_address.to_s.start_with? 'bounce')
+        Rails.logger.info "Received incoming mail correctly"
         BounceReceiver.receive_bounced_email(message.to_s, to_address.to_s)
         render :text => 'success', :status => 200
       else
+        Rails.logger.info "Message failed #{message} from incorrect to address: #{to_address}"
         render :text => "Message failed #{message} from incorrect to address: #{to_address}", :status => 404
       end
     end
