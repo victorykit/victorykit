6a0d76a77f2f997d680b26532a5f63039a9a510c
Aaron Swartz <me@aaronsw.com>
2012-05-17 09:40:53 -0400

    Revert "Added connection to local db for testing"
    This reverts commit 14bd5a1bc0561a97fa4ebb263f98cef41598d59a.

--- app/services/demand_progress_gateway.rb
+++ app/services/demand_progress_gateway.rb
@@ -1,7 +1,7 @@
 class DemandProgressGateway < ActiveRecord::Base
   self.abstract_class = true
   
-  establish_connection :development
+  establish_connection(Settings.demand_progress.db_uri)
 end
 
 class UnsubscribeRequest
