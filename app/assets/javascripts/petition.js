b11b480c04ac06fc9680f526ca98109d3e5936d3
rachel kammerer <rlkammerer@gmail.com>
2012-05-22 17:06:51 -0400

    added tests for share petition

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -1,10 +1,13 @@
 jQuery(function(){
-	var cookie = $.cookie('signed_petitions') || '';	
+	var cookie = $.cookie('signed_petitions') || '';
 	var petitionIds = cookie.split("|");
 	var currentPetitionId = $('#petitionId').val();
 	if ($.inArray(currentPetitionId, petitionIds) > -1) {
-        $('#thanks-for-signing-message').show();
-        $('#sign-up-form').hide();
-    }
-    //$('#petition_description').wysihtml5();
+    $('#thanks-for-signing-message').show();
+    $('#sign-up-form').hide();
+    $('#thanksModal').modal('toggle');
+  }
+  //$('#petition_description').wysihtml5();
+
 });
+
