25abfa3c499a70e882f43bb9f7001dba4568373b
Michelle D'Souza <michd2005@gmail.com>
2012-05-22 10:57:42 -0600

    Revert "Revert "Added settings file for development & test""
    This reverts commit f77ec3d7f8fb7d753b975092d81de4b7faeadedc.

--- app/services/demand_progress_gateway.rb
+++ app/services/demand_progress_gateway.rb
@@ -1,5 +1,5 @@
 class DemandProgressGateway < ActiveRecord::Base
-  #self.abstract_class = true
+  self.abstract_class = true
   
-  #establish_connection(Settings.demand_progress.db_uri)
+  establish_connection({:adapter => "mysql2", :host => Settings.demand_progress.host, :username => Settings.demand_progress.username, :password => Settings.demand_progress.password, :database => Settings.demand_progress.database})
 end
