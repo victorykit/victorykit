7fd0ec7305844a8d454d9fa1fb71b4ddb896ac67
Michelle D'Souza <michd2005@gmail.com>
2012-05-22 13:55:31 -0600

    Revert "Added core action locally and checked in development.yml & test.yml"
    This reverts commit 770c3a220c1f89e261ff5550e6ff778e809663d0.

--- db/migrate/20120522191648_create_core_action_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateCoreActionTable < ActiveRecord::Migration
-  def change
-    create_table :core_action do |t|
-      t.string :email
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
