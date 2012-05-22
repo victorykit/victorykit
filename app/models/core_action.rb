05bfafb10968c818d0e39181aab45a5b01818750
Michelle D'Souza <michd2005@gmail.com>
2012-05-22 09:54:06 -0600

    Added settings file for development & test

--- app/models/core_action.rb
+++ app/models/core_action.rb
@@ -1,5 +1,4 @@
 class CoreAction < DemandProgressGateway 
-=begin
   self.table_name = 'core_action'
   
   def self.fetch_unsubscribers_since(last_updated)  
@@ -12,5 +11,5 @@ class CoreAction < DemandProgressGateway
     
     @core_actions.map { |x| UnsubscribeRequest.new(x["email"], x["created_at"]) }
   end
-=end
+  
 end
