d4ad51ec5ff156d8acb91800ff6ad8cd09c26fcb
Michelle D'Souza <michd2005@gmail.com>
2012-05-22 10:57:39 -0600

    Revert "Revert "Fixed issue with nil date for last created member""
    This reverts commit 5e354296afc7ca3e838444a623a6b59c1dcaed69.

--- worker/unsubscribe_poller.rb
+++ worker/unsubscribe_poller.rb
@@ -15,7 +15,8 @@ class UnsubscribePoller
         lock_update_token(update_token, true)
         unsubscribe_requests = [] #CoreAction.fetch_unsubscribers_since(update_token.updated_at)
         unsubscribe_requests.each {|r| r.unsubscribe_member}
-        update_token.updated_at = unsubscribe_requests.max { |x,y| x.created_at <=> y.created_at }.created_at
+        latest_member_created = unsubscribe_requests.max { |x,y| x.created_at <=> y.created_at }
+        update_token.updated_at = latest_member_created.nil? ? update_token.updated_at : latest_member_created.created_at
       rescue => error
         puts "Error in retrieving and unsubscribing users #{error} #{error.backtrace.join}"
       ensure
