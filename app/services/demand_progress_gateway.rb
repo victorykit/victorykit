ee33b0c514b5452e88cb02a7a73d672114997f13
Michelle D'Souza <michd2005@gmail.com>
2012-05-23 14:39:43 -0600

    Revert "Changed connection string in DP Gateway to string"
    This reverts commit 13301fa6caebf6aa6a0e3c88607639b1c3fc9278.

--- app/services/demand_progress_gateway.rb
+++ app/services/demand_progress_gateway.rb
@@ -1,5 +1,5 @@
 class DemandProgressGateway < ActiveRecord::Base
   self.abstract_class = true
   
-  establish_connection "demand_progress_" + ::Rails.env.to_s
+  establish_connection "demand_progress_"[::Rails.env]
 end
