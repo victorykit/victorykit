4bedd9abefb6638a2b901b0191f373d21be9e3aa
Jim Arnold <jim@thoughtworks.com>
2012-05-24 15:34:52 -0400

    fixed element selectors

--- spec/smoke/petition_signing.rb
+++ spec/smoke/petition_signing.rb
@@ -1,18 +1,19 @@
 require 'smoke_spec_helper.rb'
 require 'uri'
 
+include Rails.application.routes.url_helpers
+
 describe 'Petition page' do
   let(:petition) {create :petition}
 
   before :each do
-    uri = URI.join(HOST_URL, "petitions/#{petition.id}").to_s
+    uri = URI.join(HOST_URL, petition_path(petition)).to_s
     $driver.navigate.to(uri)
-    puts $driver.page_source
   end
 
   it 'should allow users to sign' do
-    type('bob').into('signature_name')
-    type('bob@bobs.com').into('signature_email')
+    type('bob').into(:id => 'signature_name')
+    type('bob@bobs.com').into(:id => 'signature_email')
     click :id => 'sign_petition'
   
     wait.until { $driver.find_element(:class => "thanks") }
