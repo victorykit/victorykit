4f8528fe9328ae8b46739a63830d689104c1aaa9
Jim Arnold <jim@thoughtworks.com>
2012-05-24 14:50:52 -0400

    another stab at getting petition UI tests working

--- spec/smoke/petition_signing.rb
+++ spec/smoke/petition_signing.rb
@@ -2,17 +2,16 @@ require 'smoke_spec_helper.rb'
 require 'uri'
 
 describe 'Petition page' do
-  pending "get it working in CI" do
-    before :each do
-      uri = URI.join(HOST_URL, 'petitions/1').to_s
-      $driver.navigate.to(uri)
-    end
-    it 'should allow users to sign' do
-      type('bob').into('signature_name')
-      type('bob@bobs.com').into('signature_email')
-      click :id => 'sign_petition'
-    
-      wait.until { $driver.find_element(:class => "thanks") }
-    end
+  before :each do
+    uri = URI.join(HOST_URL, 'petitions/1').to_s
+    $driver.navigate.to(uri)
+    puts $driver.page_source
+  end
+  it 'should allow users to sign' do
+    type('bob').into('signature_name')
+    type('bob@bobs.com').into('signature_email')
+    click :id => 'sign_petition'
+  
+    wait.until { $driver.find_element(:class => "thanks") }
   end
 end
