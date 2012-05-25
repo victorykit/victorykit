f37e337e1f4138d442816c68074d205e209f014a
Michelle D'Souza <michd2005@gmail.com>
2012-05-24 21:02:42 -0600

    Moved connection settings to mysql from database.yml to settings.yml

--- app/services/demand_progress_gateway.rb
+++ app/services/demand_progress_gateway.rb
@@ -1,16 +1,14 @@
-class DemandProgressGateway < ActiveRecord::Base
-  self.abstract_class = true
+class DemandProgressGateway
   
   def self.fetch_unsubscribers_since(date)
-  	config = Rails.configuration.database_configuration["dp_production"] 
-  	if config.nil?
-  		Rails.logger.info "Config is nil"
-  	end
-  	
-  	client = Mysql2::Client.new(:host => config["host"], :username => config["username"], :password => config["password"], :database => config["database"])
+  	client = Mysql2::Client.new(:host => settings.host, :username => settings.username, :password => settings.password, :database => settings.database)
   	results = client.query("select email, core_action.created_at from core_action join core_unsubscribeaction on (core_action.id = core_unsubscribeaction.action_ptr_id) join core_user on (core_user.id = core_action.user_id) where core_action.created_at > '" + date.to_s + "' order by created_at desc")
 
   	results.to_a.map{ |row| UnsubscribeRequest.new(row["email"], row["created_at"]) }
+  end
+  
+  def self.settings
+    Settings.dp_connection
   end
 
 end
