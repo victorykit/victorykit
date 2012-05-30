d70f5d1e6bb0bf6bca591728b06de45e8fb09e3f
Michelle D'Souza <michd2005@gmail.com>
2012-05-30 16:03:24 -0600

    [AddMailCheck Branch] Added more domains to check against & the ability for user to click suggested email address

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-	var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
+	var domains = ['hotmail.com', 'gmail.com', 'aol.com', 'yahoo.com','msn.com','comcast.net','bellsouth.net','verizon.net','earthlink.net','cox.net','rediffmail.com','yahoo.ca','btinternet.com','charter.net','shaw.ca','ntlworld.com'];
 	var $email = $('#signature_email');
 	var $hint = $("#hint");
+
+	$('#suggested_email').live("click",function(){
+        $email.val($(this).html());
+        $hint.css('display', 'none');
+        return false;
+    });
+
 	$email.blur(function() {
-	  $hint.css('display', 'none'); // Hide the hint
+	  $hint.css('display', 'none');
 	  $email.mailcheck({
 	  	domains: domains,
 	    suggested: function(element, suggestion) {
 	       	if(!$hint.html()) {
-	        var suggestion = "Did you mean <b>" + suggestion.full + "</b>?";              
-	        $hint.html(suggestion).fadeIn(150);
+	        var suggestion = "Did you mean <a href='#' id='suggested_email'>" + suggestion.full + "</a>?";              
+	     	   $hint.html(suggestion).fadeIn(150);
 	      }
 	    }
 	  });
