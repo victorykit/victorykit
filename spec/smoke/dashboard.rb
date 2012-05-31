6324d819593e571077cc10b5ffe9f111046e3a46
Jim Arnold <jim@thoughtworks.com>
2012-05-31 09:52:19 -0400

    removed debug code from a test

--- spec/smoke/dashboard.rb
+++ spec/smoke/dashboard.rb
@@ -11,9 +11,6 @@ describe "statistics dashboard" do
 	  wait_for_ajax
 	  doc = Nokogiri::HTML($driver.page_source)
 
-	  go_to 'admin/petitions.json'
-	  json = Nokogiri::HTML($driver.page_source)
-
 	  row_count = doc.xpath("count(//table/tbody/tr)")
 	  row_count.should > 0
 	end
