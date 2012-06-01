836b53338126dc0e2059b6a8b7dab1517e5da781
Jim Arnold <jim@thoughtworks.com>
2012-06-01 12:13:30 -0400

    truncating long user agent strings before saving

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -4,4 +4,10 @@ class Signature < ActiveRecord::Base
   belongs_to :member
   validates_presence_of :name
   validates :email, :presence => true, :email => true
+
+  before_save :truncate_user_agent
+
+	def truncate_user_agent
+	  self.user_agent = self.user_agent[0..254]
+	end
 end
--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Signature do
-  describe "validation" do
+  context "validation" do
     subject { build(:signature) }
     it { should validate_presence_of :email }
     it { should validate_presence_of :name }
     it_behaves_like "email validator"
+  end
+
+  context "given a really long user agent" do
+  	it "truncates it to 255 characters" do
+  		signature = build(:signature, user_agent: "0" * 512)
+  		signature.save!
+  		signature.user_agent.length.should == 255
+  	end
   end
 end
