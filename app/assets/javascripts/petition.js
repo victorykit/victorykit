e420b46e9f809e034a7eaba69bfaa2395f1ebdcf
Michelle D'Souza <michd2005@gmail.com>
2012-06-01 14:57:22 -0600

    Added mailcheck suggestion when enter is pressed in signature form

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -3,24 +3,31 @@ $(document).ready(function() {
 	var $email = $('#signature_email');
 	var $hint = $("#hint");
 
-	$('#suggested_email').live("click",function(){
+	$('.suggested_email').live("click",function() {
         $email.val($(this).html());
         $hint.css('display', 'none');
-        return false;
+        return false; 
     });
 
-	$email.blur(function() {
-	  $hint.css('display', 'none');
-	  $email.mailcheck({
+	$('form').on("submit", function(event) {
+		mailCheckSuggestions(event);
+		return event.go;
+    });
+
+	function mailCheckSuggestions(event) {
+		$hint.css('display', 'none');
+	  	$email.mailcheck({
 	  	domains: domains,
 	    suggested: function(element, suggestion) {
+	    	event.go = true;
 	       	if(!$hint.html()) {
-	        var suggestion = "Did you mean <a href='#' id='suggested_email'>" + suggestion.full + "</a>?";              
-	     	   $hint.html(suggestion).fadeIn(150);
+	        	var suggestion = 'Did you mean <a href="#" id="suggested_email" class="suggested_email">' + suggestion.full + "</a>?";              
+	     	   	$hint.html(suggestion).fadeIn(150);
+      			event.go = false;
 	      }
 	    }
 	  });
-	});
+	}
 });
 
 jQuery(function(){
