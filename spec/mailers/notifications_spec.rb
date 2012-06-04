de7044acf54abf47d2fab7008acaa387f755bde5
Jim Arnold <jim@thoughtworks.com>
2012-06-04 17:06:53 -0400

    added unsubscribe link to thanks-for-signing email

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Notifications do
   describe "signed_petition" do
     let(:signature){ create(:signature)}
+    let(:unsubscribe_link){"http://test/unsubscribes/new"}
     let(:mail) { Notifications.signed_petition(signature) }
     it "renders the headers" do
       mail.subject.should match /#{signature.petition.title}/
@@ -11,6 +12,7 @@ describe Notifications do
 
     it "renders the body" do
       mail.body.encoded.should include(signature.petition.title)
+      mail.body.encoded.should include(unsubscribe_link)
     end
   end
 end
