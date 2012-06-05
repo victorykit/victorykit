5c2a5ace4a56e2baa42a145619d9f5e1879637d2
Jim Arnold <jim@thoughtworks.com>
2012-06-05 12:01:09 -0400

    added user agent to status page

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,5 +4,6 @@ class StatusController < ApplicationController
     session[:tmp] = 1
     session.delete(:tmp)
     @session_id = request.session_options[:id]
+	  @user_agent = request.env['HTTP_USER_AGENT']
   end
 end
