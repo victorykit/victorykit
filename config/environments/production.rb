fe2e9e444c3d835fa4dda3bcba04ba63a71b79d2
Mykola Kurutin <mkurutin@thoughtworks.com>
2012-06-07 20:12:37 -0400

    added application_ie.css to precompiled list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Victorykit::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( application_ie.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
