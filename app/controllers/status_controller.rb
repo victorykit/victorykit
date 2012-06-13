366db030bab07c518b1b80ad4031fb99f967e562
Martin Andersen <manderdev@gmail.com>
2012-06-12 21:51:44 -0600

    added ability to update session from status page if providing req var matching an env var

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,6 +4,19 @@ class StatusController < ApplicationController
     session[:tmp] = 1
     session.delete(:tmp)
     @session_id = request.session_options[:id]
-	  @user_agent = request.env['HTTP_USER_AGENT']
+    @user_agent = request.env['HTTP_USER_AGENT']
+
+    @can_update_session = can_update_session?
+
+    if @can_update_session
+      params.each do |p|
+        key = p[0].gsub('__', ' ')
+        if session[key] then session[key] = p[1] end
+      end
+    end
+  end
+
+  def can_update_session?
+    return (params['debug_token'] == ENV['VK_DEBUG_TOKEN'])
   end
 end
