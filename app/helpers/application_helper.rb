8b951a8c4b39ed849f5d5940927536a1e01b65c7
rachel kammerer <rlkammerer@gmail.com>
2012-06-14 12:51:58 -0400

    make facebook suck less (mostly by making it only have the like + box with number in both modal and sidebar)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,11 +14,9 @@ module ApplicationHelper
     end
   end
 
-  def fb_like(url, ref_hash, classes = nil, is_button_count = false, is_in_modal = false)
-    attributes = {href: url, send: false, show_faces: false, action: 'like', ref: ref_hash}
-    attributes.merge!({layout: 'button_count'}) if is_button_count
-    attributes.merge!({width: '215'}) if is_in_modal
-    attributes.merge!({width: '270'}) if !is_in_modal
+  def fb_like(url, ref_hash, classes = nil, is_button_count = false)
+    attributes = {href: url, send: false, show_faces: false, action: 'like', ref: ref_hash, width: '270'}
+    attributes.merge!({layout: 'button_count', width: '100'}) if is_button_count
     tag "fb:like", {data: attributes, class: classes}, false, true
   end
 
