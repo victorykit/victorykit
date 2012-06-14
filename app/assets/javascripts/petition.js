1bb3f53d1e975e41e00cefeb6b4aa921a77d019a
Jim Arnold <jim@thoughtworks.com>
2012-06-14 16:15:31 -0400

    added a friendly hint to re-sign the form if their email address is correct

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -71,7 +71,8 @@ function EmailSuggestions() {
       suggested: function(element, suggestion) {
         event.go = true;
         if(!$hint.html()) {
-          var suggestion = 'Did you mean <a href="#" id="suggested_email" class="suggested_email">' + suggestion.full + "</a>?";
+          var suggestion = 'Did you mean <a href="#" id="suggested_email" class="suggested_email">' + suggestion.full + "</a>?" +
+              "<br/>Click the '" + $("#sign_petition").val() + "' button again if your address is correct";
           $hint.html(suggestion).fadeIn(150);
           event.go = false;
         }
