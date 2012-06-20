6a66e7a21879c6fd593deb17e70b2e53f6037bbd
Jim Arnold <jim@thoughtworks.com>
2012-06-20 10:26:39 -0400

    added tests for recording likes

---
+++ spec/controllers/social_tracking_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe SocialTrackingController do
+  describe "GET new" do
+    it "records a like on a petition" do
+	    petition = create(:petition)
+
+      get(:new, {petition_id: petition.id})
+      like = Like.last
+	    like.petition.should == petition
+	    like.member.should be_nil
+    end
+    it "records a like by a member on a petition" do
+      petition = create(:petition)
+      signature = create(:signature)
+
+      get(:new, {petition_id: petition.id, signature_id: signature.id})
+      like = Like.last
+      like.petition.should == petition
+      like.member.should == signature.member
+    end
+  end
+end
