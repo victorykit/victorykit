952d40651fb56f775d111ad547b25d699f0293f1
Jim Arnold <jim@thoughtworks.com>
2012-06-21 11:06:02 -0400

    added a subscribed? method on Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,7 +6,18 @@ class Member < ActiveRecord::Base
   validates :name, :presence => true
 
   def self.random_and_not_recently_contacted
-    q = Member.connection.execute("SELECT members.id FROM members LEFT JOIN sent_emails ON (members.id = sent_emails.member_id AND sent_emails.created_at > now() - interval '1 week') LEFT JOIN unsubscribes ON (members.id = unsubscribes.member_id) WHERE sent_emails.member_id is null AND unsubscribes.member_id is null").to_a
-    q.empty? ? nil : Member.find_by_id(q.sample['id'])
+	  uncontacted_members = Member.joins("LEFT JOIN sent_emails ON (members.id = sent_emails.member_id AND sent_emails.created_at > now() - interval '1 week') WHERE sent_emails.member_id is null")
+	  target_members = uncontacted_members.select {|m|m.subscribed?}
+	  target_members.sample
   end
+
+	def subscribed?
+		last_subscribed = subscribes.maximum("created_at")
+		last_unsubscribed = unsubscribes.maximum("created_at")
+
+		return true if last_unsubscribed.nil?
+		return false if last_subscribed.nil?
+
+		last_subscribed > last_unsubscribed
+	end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -8,9 +8,9 @@ describe Member do
       
       Member.random_and_not_recently_contacted.should be nil
     end
-    it "should select members contacted more than a month ago" do
+    it "should select members contacted more than a week ago" do
       previously_contacted_member = create :member
-      create :sent_email, created_at: 60.days.ago, member: previously_contacted_member
+      create :sent_email, created_at: 8.days.ago, member: previously_contacted_member
 
       Member.random_and_not_recently_contacted.should eq previously_contacted_member
     end
@@ -21,4 +21,33 @@ describe Member do
       Member.random_and_not_recently_contacted.should be nil
     end
   end
+	describe "subscribed" do
+		it "is subscribed if subscribe date greater than unsubscribe date" do
+		  subscribed_member = create :member
+		  create :unsubscribe, {member: subscribed_member, created_at: 2.days.ago}
+		  create :subscribe, {member: subscribed_member, created_at: 1.day.ago}
+
+			subscribed_member.should be_subscribed
+		end
+		it "is subscribed if there is no unsubscribe" do
+			subscribed_member = create :member
+		  create :subscribe, {member: subscribed_member, created_at: 1.day.ago}
+
+			subscribed_member.should be_subscribed
+		end
+		it "is unsubscribed if unsubscribe date greater than subscribe date" do
+			subscribed_member = create :member
+			create :subscribe, {member: subscribed_member, created_at: 2.day.ago}
+			create :unsubscribe, {member: subscribed_member, created_at: 1.days.ago}
+
+			subscribed_member.should_not be_subscribed
+		end
+		it "is unsubscribed if there is only an unsubscribe but no subscribe" do
+			#remove when we add a subscribe record when member is created
+			subscribed_member = create :member
+			create :unsubscribe, {member: subscribed_member, created_at: 1.days.ago}
+
+			subscribed_member.should_not be_subscribed
+		end
+	end
 end
