2558301ab5041b58a32c60e4e350890a86a3a3d8
Jim Arnold <jim@thoughtworks.com>
2012-06-21 17:04:04 -0400

    reverting to SQL join - ActiveRecord is crazy slow here for some reason

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,11 +8,13 @@ class Member < ActiveRecord::Base
   validates :name, :presence => true
 
   def self.random_and_not_recently_contacted
-	  uncontacted_members = Member.joins("LEFT JOIN sent_emails ON (members.id = sent_emails.member_id AND sent_emails.created_at > now() - interval '1 week') WHERE sent_emails.member_id is null")
+	  uncontacted_members = Member.connection.execute("SELECT members.id FROM members LEFT JOIN sent_emails ON (members.id = sent_emails.member_id AND sent_emails.created_at > now() - interval '1 week') WHERE sent_emails.member_id is null").to_a
 	  subscribe_dates = Subscribe.group(:member_id).maximum(:created_at)
 	  unsubscribe_dates = Unsubscribe.group(:member_id).maximum(:created_at)
-    subscribers = uncontacted_members.select {|m| active_subscription?(subscribe_dates[m.id], unsubscribe_dates[m.id])}
-	  subscribers.sample
+    subscribed_members = uncontacted_members.select {|m| active_subscription?(subscribe_dates[m['id'].to_i], unsubscribe_dates[m['id'].to_i])}
+
+	  return nil if subscribed_members.empty?
+	  Member.find(subscribed_members.sample['id'])
   end
 
   def self.active_subscription?(subscribe_date, unsubscribe_date)
