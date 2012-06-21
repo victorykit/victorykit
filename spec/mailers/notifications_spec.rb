cc0064eac7335b9a44bb84adfbbef104129be200
Jim Arnold <jim@thoughtworks.com>
2012-06-21 14:59:16 -0400

    added prettier urls for [un]subscribing

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Notifications do
   describe "signed_petition" do
     let(:signature){ create(:signature)}
-    let(:unsubscribe_link){"http://test/unsubscribes/new"}
+    let(:unsubscribe_link){"http://test/unsubscribe"}
     let(:mail) { Notifications.signed_petition(signature) }
     it "renders the headers" do
       mail.subject.should match /#{signature.petition.title}/
@@ -17,7 +17,7 @@ describe Notifications do
   end
 
 	describe "unsubscribed" do
-		let(:signup_link){"http://test/login"}
+		let(:signup_link){"http://test/subscribe"}
 		let(:unsubscribe){create(:unsubscribe)}
 		let(:mail) { Notifications.unsubscribed(unsubscribe) }
 		it "should include a link to sign up" do
