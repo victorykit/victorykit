63aeb00f1b26b5d4544fb81802a34bb5b8524c24
rachel kammerer <rlkammerer@gmail.com>
2012-06-27 10:40:31 -0400

    style the add fb/email subject lines on start/edit petition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,7 +67,7 @@ module ApplicationHelper
   end
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy)
   end
 
   def link_to_add_fields(name, f, association, html_options, locals)
