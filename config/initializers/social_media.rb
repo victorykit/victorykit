e43b6f000d9b1d4ed325003b933828d256f98bfe
Mykola Kurutin <mkurutin@thoughtworks.com>
2012-06-29 17:05:51 -0400

    moved facebook app id to env variable

--- config/initializers/social_media.rb
+++ config/initializers/social_media.rb
@@ -1,1 +1,1 @@
-Victorykit::Application.config.social_media = YAML.load_file(Rails.root.join('config', 'social_media.yml'))[Rails.env].with_indifferent_access
+Victorykit::Application.config.social_media = YAML.load(ERB.new(File.read(Rails.root.join('config', 'social_media.yml'))).result)[Rails.env].with_indifferent_access
