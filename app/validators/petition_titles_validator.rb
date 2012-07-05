cffce5007f7f7faac4336215e8e1bc2f228267fe
Martin Andersen <manderdev@gmail.com>
2012-07-05 17:38:55 -0600

    standardized methods text and title on title in PetitionTitle

--- app/validators/petition_titles_validator.rb
+++ app/validators/petition_titles_validator.rb
@@ -6,8 +6,8 @@ class PetitionTitlesValidator < ActiveModel::Validator
   def validate_unique_by_title_type(record)
     PetitionTitle.types.each do |type|
       alt_titles = record.petition_titles.select { |pt| pt.title_type == type }
-      texts = alt_titles.map {|pt| pt.text}
-      record.errors[:base] << "#{PetitionTitle.full_name(type)} must be unique" if texts.count != texts.uniq.count
+      titles = alt_titles.map {|pt| pt.title}
+      record.errors[:base] << "#{PetitionTitle.full_name(type)} must be unique" if titles.count != titles.uniq.count
     end
   end
 end
