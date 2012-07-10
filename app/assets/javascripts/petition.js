7bf470848ea9e402159ba1fdf044f747a40829f0
Jim Arnold <jim@thoughtworks.com>
2012-07-10 11:08:08 -0400

    checking if user is logged in to fb before initializing app

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
   initTwitter();
   initTabIndexes();
   if (VK.is_facebook_sharing_enabled === "true") {
-    FB.init({
-      appId      : $('meta[property="fb:app_id"]').attr("content"),
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
+    FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
 
-    FB.login(function (response) {
-      if (response.authResponse) {
+        FB.init({
+          appId:$('meta[property="fb:app_id"]').attr("content"),
+          status:true, // check login status
+          cookie:true, // enable cookies to allow the server to access the session
+          xfbml:true  // parse XFBML
+        });
+
         if (VK.fb_action_instance_id !== "") {
           FB.api(VK.fb_action_instance_id, 'get', function (response) {
             /*
@@ -45,7 +46,7 @@ $(document).ready(function() {
   preventWhitespaceOn('#signature_email');
   applyRichTextEditorTo('#petition_description');
 
-  $('form').on("submit", function(event) {
+  $('form').on("submit", function (event) {
     if (!VK.signing_from_email) {
       var emailSuggestor = new EmailSuggestions();
       emailSuggestor.init();
@@ -59,7 +60,7 @@ $(document).ready(function() {
     $('#email_subject_link').hide();
   }
 
-  $('#email_subject_link').click(function() {
+  $('#email_subject_link').click(function () {
     $('#email_subject').show();
     $('#email_subject input').focus();
     $('#email_subject_link').hide();
@@ -79,33 +80,34 @@ $(document).ready(function() {
 function setupSocialTracking() {
   try {
     if (FB && FB.Event && FB.Event.subscribe) {
-      FB.Event.subscribe('edge.create', function(targetUrl) {
+      FB.Event.subscribe('edge.create', function (targetUrl) {
         _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
         //Google doesn't export social event data yet, so we have to track social actions as events too
         _gaq.push(['_trackEvent', 'facebook', 'like', targetUrl]);
         $.ajax({
-          url: VK.social_tracking_url,
-          data: setUpParamsForSocialTracking('like','')
+          url:VK.social_tracking_url,
+          data:setUpParamsForSocialTracking('like', '')
         });
         if (!$('.fb_share.btn').is(":visible")) {
           $('.tweet').show();
         }
       });
-      FB.Event.subscribe('edge.remove', function(targetUrl) {
+      FB.Event.subscribe('edge.remove', function (targetUrl) {
         _gaq.push(['_trackSocial', 'facebook', 'unlike', targetUrl]);
         _gaq.push(['_trackEvent', 'facebook', 'unlike', targetUrl]);
       });
     }
-  } catch (e) {}
+  } catch (e) {
+  }
 }
 
 function setUpParamsForSocialTracking(facebook_action, action_id) {
-  var params = {petition_id: VK.petition_id, facebook_action: facebook_action};
+  var params = {petition_id:VK.petition_id, facebook_action:facebook_action};
   if (VK.signature_id != "") {
-    params = $.extend(params, {signature_id: VK.signature_id});
+    params = $.extend(params, {signature_id:VK.signature_id});
   }
-  if(action_id != ""){
-    params = $.extend(params, {action_id: action_id}); 
+  if (action_id != "") {
+    params = $.extend(params, {action_id:action_id});
   }
 
   return params;
@@ -113,7 +115,7 @@ function setUpParamsForSocialTracking(facebook_action, action_id) {
 
 function setupShareFacebookButton() {
   var shareButton = $('.fb_share.btn');
-  shareButton.click(function(event) {
+  shareButton.click(function (event) {
     shareButton.hide();
     $('.fb_share_message').text("Connecting to Facebook...");
     $('.fb_share_message').show();
@@ -123,20 +125,20 @@ function setupShareFacebookButton() {
 
 function submitFacebookAction() {
   FB.init({
-    appId      : $('meta[property="fb:app_id"]').attr("content"),
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
+    appId:$('meta[property="fb:app_id"]').attr("content"),
+    status:true, // check login status
+    cookie:true, // enable cookies to allow the server to access the session
+    xfbml:true  // parse XFBML
   });
 
-  FB.login(function(response) {
+  FB.login(function (response) {
     if (response.authResponse) {
       console.log('Welcome!  Fetching your information.... ');
       FB.api(
         '/me/watchdognet:sign',
         'post',
         {
-          petition: $('meta[property="og:url"]').attr("content")
+          petition:$('meta[property="og:url"]').attr("content")
         },
         function (response) {
           if (!response || response.error) {
@@ -146,8 +148,8 @@ function submitFacebookAction() {
             $('.fb_share_message').text("Please try again.");
           } else {
             $.ajax({
-              url: VK.social_tracking_url,
-              data: setUpParamsForSocialTracking('share', response.id)
+              url:VK.social_tracking_url,
+              data:setUpParamsForSocialTracking('share', response.id)
             });
             inviteToShareOnTwitter();
           }
@@ -158,7 +160,7 @@ function submitFacebookAction() {
       $('.fb_share_message').hide();
       console.log('User cancelled login or did not fully authorize.');
     }
-  }, {scope: 'publish_actions'});
+  }, {scope:'publish_actions'});
 }
 
 function inviteToShareOnTwitter() {
@@ -185,7 +187,9 @@ function applyRichTextEditorTo(item) {
 }
 
 function preventWhitespaceOn(input) {
-  $(input).change(function() { this.value = this.value.replace(/ /g, ''); });
+  $(input).change(function () {
+    this.value = this.value.replace(/ /g, '');
+  });
 }
 
 function EmailSuggestions() {
@@ -193,8 +197,8 @@ function EmailSuggestions() {
   var $email = $('#signature_email');
   var $hint = $("#hint");
 
-  this.init = function() {
-    $('.suggested_email').live("click",function() {
+  this.init = function () {
+    $('.suggested_email').live("click", function () {
       $email.val($(this).html());
       $hint.css('display', 'none');
       return false;
@@ -205,14 +209,14 @@ function EmailSuggestions() {
     $hint.css('display', 'none');
     $email.mailcheck({
       //annoyingly, mailcheck doesn't let you add to their default list of domains, so we have to duplicate them all here.
-      domains: ["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com",
-                "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
-                "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com", "q.com"],
-      suggested: function(element, suggestion) {
+      domains:["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com",
+        "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
+        "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com", "q.com"],
+      suggested:function (element, suggestion) {
         event.go = true;
         if (!$hint.html()) {
           var suggestion = 'Did you mean <a href="#" id="suggested_email" class="suggested_email">' + suggestion.full + "</a>?" +
-              "<br/>Click the '" + $("#sign_petition").val() + "' button again if your address is correct";
+            "<br/>Click the '" + $("#sign_petition").val() + "' button again if your address is correct";
           $hint.html(suggestion).fadeIn(150);
           event.go = false;
         }
@@ -222,12 +226,14 @@ function EmailSuggestions() {
 }
 
 function initTwitter() {
-  !function(d,s,id) {
-    var js,fjs=d.getElementsByTagName(s)[0];
-    if(!d.getElementById(id)) {
-      js=d.createElement(s);
-      js.id=id;js.src="//platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js,fjs);}
+  !function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
     }
-    (document,"script","twitter-wjs");
+  }
+    (document, "script", "twitter-wjs");
 }
