397f849382fe016e35c95515ff288bd064c8d287
Jim Arnold <jim@thoughtworks.com>
2012-07-10 11:00:01 -0400

    *really* hding facebook share button if user has already shared

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -162,6 +162,7 @@ function submitFacebookAction() {
 }
 
 function inviteToShareOnTwitter() {
+  $('.fb_share.btn').hide();
   $('.fb_share_message').hide();
   $('.tweet').show();
   $('#thanks-for-signing-message .share').text("You shared on Facebook! How about Twitter?");
