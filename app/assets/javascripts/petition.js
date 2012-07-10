506c9fab34dd3347f0e6eef72110b899762976e1
Mykola Kurutin <mkurutin@thoughtworks.com>
2012-07-10 15:37:32 -0400

    fixed modal sharing message

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -138,7 +138,7 @@ function inviteToShareOnTwitter() {
   $('.fb_share.btn').hide();
   $('.fb_share_message').hide();
   $('.tweet').show();
-  $('#thanks-for-signing-message .share').text("You shared on Facebook! How about Twitter?");
+  $('.sharing-message').text("You shared on Facebook! How about Twitter?");
 }
 
 function initTabIndexes() {
