fcd07d804f14dcb4dfe1374e90b572f0542d1265
rachel kammerer <rlkammerer@gmail.com>
2012-07-11 17:18:39 -0400

    Revert "add correct class to test"
    This reverts commit 54ca35e13398cc39ddd3bfe90c08f38abeb48aff.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
     $("form:not(.filter) :input:visible:enabled:first").focus();
-    $('#nav_btn').click(function() {
-      $('.nav').toggle();
-      $('.navigation .row').toggleClass('grey');
-      $('#nav_btn i').toggleClass('icon-plus');
-      $('#nav_btn i').toggleClass('icon-minus');
-    });
 });
--- spec/smoke/petition_creation.rb
+++ spec/smoke/petition_creation.rb
@@ -5,7 +5,7 @@ describe 'Petition create page' do
   it 'should allow users to create a petition' do
     petition = create_a_petition
     go_to petition_path(petition)
-    element(:class => "petition_title").text.downcase.should == petition.title
+    element(:class => "petition_title").text.should == petition.title
     element(:class => "description").text.should == petition.description
   end
   it 'should redirect to petition create page after logging in' do
