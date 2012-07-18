c6fe431908357b534111a279cd3b9798f4d8e934
mkurutin <mkurutin@thoughtworks.com>
2012-07-18 10:21:58 -0400

    fixed parsing of json from SNS

--- app/controllers/bounces_controller.rb
+++ app/controllers/bounces_controller.rb
@@ -1,41 +1,49 @@
 class BouncesController < ApplicationController
 
   def create
-    data = JSON.parse(request.raw_post)
-
-    notification_type = data["notificationType"]
-    if notification_type == "Bounce"
-      bounce_type = data["bounce"]["bounceType"]
-      bounce_sub_type = data["bounce"]["bounceSubType"]
-      email = data["bounce"]["bouncedRecipients"][0]["emailAddress"]
-
-      unsubscribe email, "#{notification_type}/#{bounce_type}/#{bounce_sub_type}"
-    elsif notification_type == "Complaint"
-      complaint_type = data["complaint"]["complaintFeedbackType"]
-      email = data["complaint"]["complainedRecipients"][0]["emailAddress"]
-      cause = "#{notification_type}"
-      cause << "/#{complaint_type}" if complaint_type
-
-      unsubscribe email, cause
+    notification = JSON.parse(request.raw_post)
+    if notification["Type"] == "SubscriptionConfirmation"
+      confirm_subscription notification
+    else
+      dsn = JSON.parse(notification["Message"])
+      process_dsn dsn
     end
-
-    if data["Type"] == "SubscriptionConfirmation"
-      AWS::SNS::Client.new.confirm_subscription(:topic_arn => data["TopicArn"], :token => data["Token"], :authenticate_on_unsubscribe => "true")
-    end
-
     render({:nothing => true})
   end
 
 
   private
-  def record_bounced_data
+  def confirm_subscription notification
+    AWS::SNS::Client.new.confirm_subscription(:topic_arn => notification["TopicArn"], :token => notification["Token"], :authenticate_on_unsubscribe => "true")
+  end
+
+  def process_dsn dsn
+      notification_type = dsn["notificationType"]
+      if notification_type == "Bounce"
+        bounce_type = dsn["bounce"]["bounceType"]
+        bounce_sub_type = dsn["bounce"]["bounceSubType"]
+        email = dsn["bounce"]["bouncedRecipients"][0]["emailAddress"]
+
+        unsubscribe email, "#{notification_type}/#{bounce_type}/#{bounce_sub_type}"
+      elsif notification_type == "Complaint"
+        complaint_type = dsn["complaint"]["complaintFeedbackType"]
+        email = dsn["complaint"]["complainedRecipients"][0]["emailAddress"]
+        cause = "#{notification_type}"
+        cause << "/#{complaint_type}" if complaint_type
+
+        unsubscribe email, cause
+      end
+
+  end
+
+  def record_dsn
     bounce = BouncedEmail.new
     bounce.raw_content = request.raw_post
     bounce.save!
   end
 
   def unsubscribe email, cause
-    record_bounced_data
+    record_dsn
     member = Member.find_by_email email
     if member
       unsubscribe = Unsubscribe.new
