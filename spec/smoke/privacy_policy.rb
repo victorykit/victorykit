a311716cfa3b4de894c13a610d324ab62bfd3cc9
rachel kammerer <rlkammerer@gmail.com>
2012-07-19 13:38:50 -0400

    layout overhaul

--- spec/smoke/privacy_policy.rb
+++ spec/smoke/privacy_policy.rb
@@ -3,6 +3,6 @@ require 'smoke_spec_helper.rb'
 describe 'Privacy policy page' do
 	it 'should have a title' do
 		go_to HOST_URL + "/privacy"
-		element(:class => "privacy_title").text.should == "Privacy Policy"
+		element(:class => "privacy_title").text.downcase.should == "privacy policy"
 	end
 end
