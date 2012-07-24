fd4e0a20c09c288531646b58ed6ba7ba3d97c4b9
mkurutin <mkurutin@thoughtworks.com>
2012-07-24 13:29:31 -0400

    added smoke test for does someone else wanna sign

--- spec/smoke/petition_signing.rb
+++ spec/smoke/petition_signing.rb
@@ -7,18 +7,20 @@ include Rails.application.routes.url_helpers
 describe 'Petition page' do
   before :each do
     @petition = create_a_petition
-    go_to petition_path(@petition)
   end
   it 'should allow users to sign' do
+    go_to petition_path(@petition)
     sign_petition
     element(:id => "thanks-for-signing-message").should be_displayed
     element(:class => "signature-form").should_not be_displayed
   end
   it 'should ensure user provides a name' do
+    go_to petition_path(@petition)
     sign_petition '', 'no@yahoo.com'
     element(:class => 'help-inline').text.should == "can't be blank"
   end
   it 'should track the referer for a signature' do
+    go_to petition_path(@petition)
     referer = create_member
     referer_hash = MemberHasher.generate(referer.id)
 
@@ -31,5 +33,16 @@ describe 'Petition page' do
 
     signature = Signature.last
     signature.referer.should == referer
+  end
+
+  it "should allow signing a petition again after clicking 'does somene else' link" do
+    force_result("full name vs first and last name" => "fullname")
+    set_default_experiment_results
+    go_to petition_path(@petition)
+
+    sign_petition
+    click(:class => "close")
+    click(:id => "sign-again-link")
+    element_exists(id: 'signature_name').should be_true
   end
 end
--- spec/smoke/signing_experiments.rb
+++ spec/smoke/signing_experiments.rb
@@ -37,39 +37,7 @@ describe "signing petitions" do
       element_exists(id: 'signature_last_name').should be_true
     end
   end
-end
-def set_default_experiment_results
-  force_result({
-    "signature display threshold" => 0,
-    "show thermometer" => 'true',
-    "seed signatures with petition creator" => "false"})
 
-    #full (I think) list of experiments that affect layout:
-    #
-    #"petition side" => "petitionleft",
-    #"thermometer and social icon placement" => "therm1",
-    #"change button color for sign petition" => "buttoncolor1",
-    #"change containing box color for sign petition" => "signaturecolor1",
-    #"change thermometer color" => "redthermometer",
-    #"change positioning and visibility of labels on sign petition form" => "signature_labels_beside_inputs_no_asterisk",
-    #"test different messaging on progress bar" => "x_signatures_of_y",
-    #"testing different widths" => "fullwidthpetition",
-    #"different background for thank you box" => "hex_f5f5f5",
-    #"different arrow colors in thank you box" => "bluearrow",
-    #"full name vs first and last name" => "fullname",
-    #"facebook sharing options" => "facebook_like",
-    #"sign button" => 'Sign!',
-    #"signature display threshold" => 0,
-    #"show thermometer" => 'true',
-    #"seed signatures with petition creator" => "false",
-    #"toggle showing vs. not showing modal" => 'false',
-    #"change layouts" => "bootstrap",
 end
 
-def force_result(params)
-  go_to "whiplash_sessions"
-  params.each do |k, v|
-    type(v).into(name: k)
-  end
-  click name: "commit"
-end
+
