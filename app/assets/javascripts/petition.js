2b8b4fde5afbfe08935d1ea380405102722ecdda
Michelle D'Souza <michd2005@gmail.com>
2012-07-25 14:08:37 -0600

    Added petition_editor.js for all petition edit page js needs

--- app/assets/javascripts/petition.js
+++ app/assets/javascripts/petition.js
@@ -97,22 +97,6 @@ function setupShareFacebookButton() {
   });
 }
 
-function initTabIndexes() {
-  $('#petition_title').attr('tabIndex', '1');
-  $('iframe').attr('tabIndex', '2');
-  if ($('#petition_to_send').length) {
-    $('#petition_to_send').attr('tabIndex', '3');
-    $('#petition_submit').attr('tabIndex', '4');
-  }
-  else {
-    $('#petition_submit').attr('tabIndex', '3');
-  }
-}
-
-function applyRichTextEditorTo(item) {
-  $(item).wysihtml5({"html": true, parserRules: wysihtml5ParserRules});
-}
-
 function preventWhitespaceOn(input) {
   $(input).change(function () {
     this.value = this.value.replace(/ /g, '');
@@ -201,41 +185,6 @@ function drawModalAfterSigning() {
   }
 }
 
-function initEditPetition() {
-  applyRichTextEditorTo('#petition_description');
-  initTabIndexes();
-  if ($('#email_subject').has('.additional_title').length) {
-    $('#email_subject').show();
-    $('#email_subject_link').hide();
-  }
-
-  $('#email_subject_link').click(function () {
-    $('#email_subject').show();
-    $('#email_subject input').focus();
-    $('#email_subject_link').hide();
-  });
-
-  if ($('#facebook_title').has('.additional_title').length) {
-    $('#facebook_title').show();
-    $('#facebook_title_link').hide();
-  }
-
-  $('#facebook_title_link').click(function () {
-    $('#facebook_title').show();
-    $('#facebook_title_link').hide();
-  });
-
-  if ($('#sharing_image').has('.additional_title').length) {
-    $('#sharing_image').show();
-    $('#sharing_image_link').hide();
-  }
-
-  $('#sharing_image_link').click(function () {
-    $('#sharing_image').show();
-    $('#sharing_image_link').hide();
-  });
-}
-
 function initShowPetition() {
   preventWhitespaceOn('#signature_email');
   setupSocialTracking();
@@ -258,16 +207,3 @@ function initSharePetition() {
   bindFacebookPopupButton();
   drawModalAfterSigning();
 }
-
-function remove_fields(link) {
-  $(link).find("input[type=hidden]").first().val("1");
-  $(link).closest(".additional_title").hide();
-}
-
-function add_fields(link, association, content, where_selector) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-  $(where_selector).append(content.replace(regexp, new_id));
-  var full_id = "#petition_#{association}_attributes_" + new_id + "_title";
-  $(full_id).focus();
-  }
