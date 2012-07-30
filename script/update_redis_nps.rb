ce3ae6628c1e0f7934931554b20af202138acbda
Aaron Swartz <me@aaronsw.com>
2012-07-30 16:09:28 -0400

    helper script for comparing redis to db

--- script/update_redis_nps.rb
+++ script/update_redis_nps.rb
@@ -2,6 +2,15 @@ def get_db_data
   #@@ get this fron the hottest_controller
 end
 
+def compare
+  db_data.each do |eid, ov| 
+    if ov != redis_data[eid]
+      puts eid
+    end
+  end
+end
+
+
 def restore_scheduled_email_nps
   get_db_data.each { |eid, ov| 
     REDIS.set("whiplash/email_scheduler_nps/#{eid}/spins", ov[0])
