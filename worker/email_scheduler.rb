995abb3ac521c716ef087dd32931c59d2fe1c261
Aaron Swartz <me@aaronsw.com>
2012-07-31 16:55:48 -0400

    send one email per member per week

--- worker/email_scheduler.rb
+++ worker/email_scheduler.rb
@@ -5,7 +5,7 @@ WEEK = 60*60*24*7
 
 class EmailScheduler
   def self.schedule_email
-    max_emails_per_week = 40000.0
+    max_emails_per_week = Member.count.to_f
     last_email = Time.new
     MailerProcessTracker.in_transaction do
       while 1
