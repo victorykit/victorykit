958d8679b0e1cbcdadecbe14157a87ec76c72e6b
Aaron Swartz <me@aaronsw.com>
2012-07-31 10:08:09 -0400

    oops, forgot to check in a file

--- spec/smoke_spec_helper.rb
+++ spec/smoke_spec_helper.rb
@@ -77,7 +77,7 @@ def force_result(params)
 end
 
 def set_default_experiment_results
-  force_result({"seed signatures with petition creator" => "false"})
+  #force_result({"seed signatures with petition creator" => "false"})
 
     #full (I think) list of experiments that affect layout:
     #
