31f5cbecfd77e773b74236e83f89321fd06e896b
mkurutin <mkurutin@thoughtworks.com>
2012-08-01 14:29:10 -0400

    rolled-back changes to petition emailer

--- spec/lib/petition_emailer_spec.rb
+++ spec/lib/petition_emailer_spec.rb
@@ -22,36 +22,8 @@ describe PetitionEmailer do
     ScheduledEmail.should_not_receive(:new_petition)
     PetitionEmailer.send
   end
-
-  context 'sending scheduled email ' do
-
-    before do
-      PetitionEmailer.should_receive(:spin!).with("email_scheduler_nps", :signatures_off_email, [petition.id.to_s()], {session_id: member.id}).and_return(petition.id)
-    end
-
-    context 'short summary is present' do
-      before do
-        petition.update_attributes(:short_summary => "cool")
-      end
-      it "should send the petition with summary if the spin returns true" do
-        should_send_email_with_summary_box true, spin:true
-      end
-      it "should not draw the summary box if short_summary is present and the spin returns false" do
-        should_send_email_with_summary_box false, spin: false
-      end
-    end
-    context 'short summary is not present' do
-      it "should not the spin and draw summary box if short_summary is not present" do
-        PetitionEmailer.should_not_receive(:spin!).with("insert summary box to emails", :signature)
-        ScheduledEmail.should_receive(:new_petition).with(petition, member, false)
-        PetitionEmailer.send
-      end
-    end
-
-    def should_send_email_with_summary_box(is_box_present, opts)
-      PetitionEmailer.should_receive(:spin!).with("insert summary box to emails", :signature).and_return(opts[:spin])
-      ScheduledEmail.should_receive(:new_petition).with(petition, member, is_box_present)
-      PetitionEmailer.send
-    end
+  it "sends the petition to the member" do
+    ScheduledEmail.should_receive(:new_petition).with(petition, member)
+    PetitionEmailer.send
   end
 end
