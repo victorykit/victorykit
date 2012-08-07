caaebb61757ab42d1db06c0306336f745ada7fd4
Vinicius Gomes <vvgomess@gmail.com>
2012-08-07 19:26:10 -0400

    using member hasher to retrieve member by hash

--- spec/lib/member_hasher_spec.rb
+++ spec/lib/member_hasher_spec.rb
@@ -2,18 +2,28 @@ require 'spec_helper'
 require 'member_hasher'
 
 describe MemberHasher do
-  it_behaves_like "a hasher"
+  
+  it_behaves_like 'a hasher'
 
-  describe "#member_for" do
-      it "should return member instance if the hash is valid and member is present" do
-        member = create(:member)
-        MemberHasher.member_for(MemberHasher.generate(member.id)).should == member
-      end
-      it "should return nil if the hash is valid but member is not present" do
-        MemberHasher.member_for(MemberHasher.generate(1)).should be_nil
-      end
-      it "should return nil if the hash is invalid" do
-        MemberHasher.member_for("invalid_hash").should be_nil
-      end
+  describe '#member_for' do
+    let(:peter_griffin) { mock }
+    
+    before do 
+      Member.stub!(:where).with(:id => 42).and_return [peter_griffin]
+      Member.stub!(:where).with(:id => 43).and_return []
+    end
+
+    context 'invalid hash' do
+      specify { MemberHasher.member_for('o_O').should be_nil }
+    end
+
+    context 'absent hashed id' do
+      specify { MemberHasher.member_for('43.HiH9s0').should be_nil }
+    end
+
+    context 'existing hased id' do
+      specify { MemberHasher.member_for('42.aCKy3f').should == peter_griffin }
+    end
+    
   end
 end
