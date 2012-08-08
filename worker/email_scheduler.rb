0c0ac489941306128b78f88df58e09463f0f2c3c
Jim Arnold <jim@thoughtworks.com>
2012-08-08 16:24:51 -0400

    fixed email scheduler - now only sleeping if we need to

--- worker/email_scheduler.rb
+++ worker/email_scheduler.rb
@@ -6,12 +6,13 @@ WEEK = 60*60*24*7
 class EmailScheduler
   def self.schedule_email
     max_emails_per_week = Member.count.to_f
-    last_email = Time.new
+
     MailerProcessTracker.in_transaction do
       while 1
+        last_email = Time.now
         PetitionEmailer.send
-        sleep(WEEK/max_emails_per_week - (Time.new-last_email))
-        last_email = Time.new
+        interval = WEEK/max_emails_per_week - (Time.now-last_email)
+        sleep(interval) unless interval < 0
       end
     end
   end
