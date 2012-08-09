8df94846354bb7eed99d43bd165558f9fedf51c2
Jim Arnold <jim@thoughtworks.com>
2012-08-09 17:02:41 -0400

    extracted some more controllers and views from the experiments page

---
+++ app/controllers/admin/stats_controller.rb
@@ -0,0 +1,82 @@
+class Admin::StatsController < ApplicationController
+  include ApplicationMetrics
+  newrelic_ignore
+  before_filter :require_admin
+
+  def browser_usage
+    signature_count = Signature.count
+    results = Signature.count(:group => "browser_name", :order => "count_all desc")
+    @browser_stat = Hash[results.map {|k,v| [k, "%2.2f" % [v.to_f*100/signature_count]]}]
+  end
+
+  def metrics
+    @hourlydata1 = sent_emails_by_part 'hour'
+    @hourlydata2 = signatures_by_part 'hour'
+    @dowdata1 = sent_emails_by_part 'dow'
+    @dowdata2 = signatures_by_part 'dow'
+    @npsdata = nps_by_day
+    @opened_emails_percentage = opened_emails_percentage
+    @clicked_email_links_percentage = clicked_email_links_percentage
+  end
+
+  def browsers
+    results = Signature
+                .select("COUNT(*) AS count_all, browser_name, date(created_at) as created_date")
+                .where('created_at > ?', 2.weeks.ago)
+                .group("browser_name, created_date")
+                .order(:created_date)
+    browsers = results.group_by &:browser_name
+    data = browsers.map do |browser, signature|
+      { label: browser, data: signature.map {|s| [ s.created_date.to_time.to_i * 1000, s.count_all ]} }
+    end
+
+    render json: data
+  end
+
+  private
+
+  def sent_emails_by_part part
+    sent_emails_by_hour = SentEmail.count(:group => "date_part('#{part}', created_at)")
+    spins = Hash[sent_emails_by_hour.map{|(k,v)| [k.to_i,v]}]
+    signed_emails_by_hour = SentEmail.count(:group => "date_part('#{part}', created_at)", :conditions => ['signature_id is not null'])
+    wins = Hash[signed_emails_by_hour.map{|(k,v)| [k.to_i,v]}]
+    [spins, wins]
+  end
+
+  def signatures_by_part part
+    q = Signature.count(:group => "date_part('#{part}', signatures.created_at)", :joins => :sent_email)
+    Hash[q.map{|(k,v)| [k.to_i,v]}]
+  end
+
+  def nps_by_day
+    sent = Hash[SentEmail.count(group: 'date(created_at)').map {|(k,v)| [k.to_date, v.to_f]}]
+    sent.default = 1
+
+    chart_for_table = Proc.new do |table, conditions=nil, subtract_unsubs=false|
+      prefs = {group: 'date(created_at)', conditions: conditions}
+      out = Hash[table.count(prefs).map {|(k,v)| [k.to_date, v.to_f]}]
+      out.default = 0
+      if subtract_unsubs
+        #NOTE: This does not include resubscribes, but there aren't many of those.
+        unsubs = Hash[Unsubscribe.count(prefs).map {|(k,v)| [k.to_date, v.to_f]}]
+        unsubs.default = 0
+      end
+      (Date.new(2012, 05, 16)..Date.today).collect do |x|
+        n = out[x]
+        n -= unsubs[x] if subtract_unsubs
+        n/sent[x]
+      end
+    end
+
+    #chart_for_table.call SentEmail, 'opened_at is not null'
+    #chart_for_table.call SentEmail, 'clicked_at is not null'
+    #chart_for_table.call SentEmail, 'signature_id is not null'
+    #chart_for_table.call Signature, 'referer_id != member_id'
+    #chart_for_table.call Signature, "user_agent like '%MSIE%'"
+    #chart_for_table.call Signature
+    #chart_for_table.call Signature, 'created_member is not true'
+
+    chart_for_table.call Member, nil, true
+  end
+
+end
