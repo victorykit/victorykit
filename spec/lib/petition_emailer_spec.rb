238745db6cd4de534c54881c650b8a8c52e720a0
Vinicius Gomes <vvgomess@gmail.com>
2012-08-09 18:35:46 -0400

    adds a way to send an email to myself from console

--- spec/lib/petition_emailer_spec.rb
+++ spec/lib/petition_emailer_spec.rb
@@ -2,28 +2,73 @@ require 'spec_helper'
 require 'petition_emailer'
 
 describe PetitionEmailer do
-  let(:petition){create(:petition, :to_send => true)}
-  let(:member){create(:member)}
-  it "asks for a random member" do
-    Member.should_receive(:random_and_not_recently_contacted).and_return(member)
-    PetitionEmailer.send
+  let(:member) { create :member }
+
+  describe '#send' do
+
+    context 'when there is a member not recently contacted' do
+      before { Member.stub!(:random_and_not_recently_contacted).and_return member }
+
+      it 'should send an email to him' do
+        PetitionEmailer.should_receive(:send_to).with(member)
+        PetitionEmailer.send
+      end
+    end
+
+    context 'when all members were recently contacted' do
+      before { Member.stub!(:random_and_not_recently_contacted) }
+
+      it 'should not send any email' do
+        PetitionEmailer.should_not_receive(:send_to)
+        PetitionEmailer.send
+      end
+    end
   end
-  it "aborts if no member is found" do
-    Member.should_receive(:random_and_not_recently_contacted).and_return(nil)
-    ScheduledEmail.should_not_receive(:new_petition)
-    PetitionEmailer.send
+
+  describe '#send_to' do
+
+    context 'when there is no interesting petition' do
+      before do 
+        Petition.stub!(:find_interesting_petitions_for).
+        with(member).and_return []
+      end
+
+      it 'should not send any email' do
+        ScheduledEmail.should_not_receive :new_petition
+        PetitionEmailer.send_to member
+      end
+    end
+
+    context 'when there are interesting petitions' do
+      let(:petitions) { [create(:petition)] }
+      let(:chosen) { petitions.first }
+
+      before do 
+        Petition.stub!(:find_interesting_petitions_for).with(member).and_return petitions
+        Petition.stub!(:find_by_id).with(chosen.id).and_return chosen
+      end
+
+      it 'should send an email to the member' do
+        PetitionEmailer.stub!(:spin_for).and_return chosen.id
+        ScheduledEmail.should_receive(:new_petition).with(chosen, member)
+        PetitionEmailer.send_to member
+      end
+
+      it 'should spin for a petition to send' do
+        ScheduledEmail.stub!(:new_petition)
+
+        experiment = 'email_scheduler_nps'
+        goal = :signatures_off_email
+        options = [chosen.id.to_s]
+        session = { session_id: member.id }
+        
+        PetitionEmailer.should_receive(:spin!).
+        with(experiment, goal, options, session).
+        and_return chosen.id.to_i
+        
+        PetitionEmailer.send_to member
+      end
+    end
   end
-  it "spins to find a petition" do
-    PetitionEmailer.should_receive(:spin!).with("email_scheduler_nps", :signatures_off_email, [petition.id.to_s], {session_id: member.id}).and_return(petition.id.to_s)
-    PetitionEmailer.send
-  end
-  it "aborts if no petitions found" do
-    PetitionEmailer.stub(:spin!).and_return nil
-    ScheduledEmail.should_not_receive(:new_petition)
-    PetitionEmailer.send
-  end
-  it "sends the petition to the member" do
-    ScheduledEmail.should_receive(:new_petition).with(petition, member)
-    PetitionEmailer.send
-  end
+
 end
