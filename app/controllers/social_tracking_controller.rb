7e971024f37e4835f6d12019a30cac2bb9e7f873
Vinicius Gomes <vvgomess@gmail.com>
2012-08-14 19:05:12 -0400

    fb widget - adding it to social tracking

--- app/controllers/social_tracking_controller.rb
+++ app/controllers/social_tracking_controller.rb
@@ -1,52 +1,67 @@
 class SocialTrackingController < ApplicationController
   def new
     win! :share
-    facebook_action = params[:facebook_action]
-    petition = Petition.find params[:petition_id]
-    member = Signature.find(params[:signature_id]).member if params[:signature_id].present?
-    action_id = params[:action_id]
-    request_id = params[:request_id]
-    friend_ids = params[:friend_ids]
-    register_facebook_like petition, member if facebook_action == 'like'
-    register_facebook_share petition, member, action_id if facebook_action == 'share'
-    register_facebook_popup_opened petition, member if facebook_action == 'popup'
-    register_facebook_request petition, member, request_id, friend_ids if facebook_action == 'request'
+    
+    @petition = Petition.find params[:petition_id]
+    @member = Signature.find(params[:signature_id]).member if params[:signature_id].present?
+    @action_id = params[:action_id]
+    @request_id = params[:request_id]
+    @friend_ids = params[:friend_ids]
+    
+    action = params[:facebook_action]
+    
+    send({
+      'like' => :register_facebook_like,
+      'share' => :register_facebook_share,
+      'popup' => :register_facebook_popup_opened,
+      'wall' => :register_facebook_wall,
+      'request' => :register_facebook_request
+    }[action])
+    
     render :text => ''
   end
 
   private
 
-  def register_facebook_like petition, member
+  def register_facebook_like
     like = Like.new
-    like.petition = petition
-    like.member = member if member.present?
+    like.petition = @petition
+    like.member = @member if @member.present?
     like.save!
   end
 
-  def register_facebook_share petition, member, action_id
+  def register_facebook_share
     share = Share.new
-    share.petition = petition
-    share.action_id = action_id if action_id.present?
-    share.member = member if member.present?
+    share.petition = @petition
+    share.action_id = @action_id if @action_id.present?
+    share.member = @member if @member.present?
     share.save!
   end
 
-  def register_facebook_popup_opened petition, member
+  def register_facebook_popup_opened
     share = Popup.new
-    share.petition = petition
-    share.member = member if member.present?
+    share.petition = @petition
+    share.member = @member if @member.present?
     share.save!
   end
 
-  def register_facebook_request petition, member, request_id, friend_ids
+  def register_facebook_wall
+    wall = FacebookWall.new
+    wall.petition = @petition
+    wall.member = @member if @member.present?
+    wall.save!
+  end
+
+  def register_facebook_request
     request = FacebookRequest.new
-    request.petition = petition
-    request.action_id = request_id if request_id.present?
-    request.member = member if member.present?
+    request.petition = @petition
+    request.action_id = @request_id if @request_id.present?
+    request.member = @member if @member.present?
     request.save!
-    friend_ids.each do |facebook_id|      
-      if member.present?  
-        new_facebook_friend = FacebookFriend.new(member_id: member.id, facebook_id: facebook_id) unless FacebookFriend.where(member_id: member.id, facebook_id: facebook_id).first.present?
+    @friend_ids.each do |facebook_id|      
+      if @member.present?  
+        friend = FacebookFriend.where(member_id: @member.id, facebook_id: facebook_id).first
+        new_facebook_friend = FacebookFriend.new(member_id: @member.id, facebook_id: facebook_id) unless friend
         new_facebook_friend.save! if new_facebook_friend.present?    
       end
     end
