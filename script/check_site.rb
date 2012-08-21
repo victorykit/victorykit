01fff143592ffb76cbddce347483146e1754d786
Jim Arnold <jim@thoughtworks.com>
2012-08-21 14:32:12 -0400

    fixed output overwriting in check_site script

--- script/check_site.rb
+++ script/check_site.rb
@@ -1,6 +1,7 @@
 while true
   status = `curl --head -s act.watchdog.net | awk 'NR==1{print $2}'`
-  if status.strip != '200'
+  status.strip!
+  if status != '200'
     10.times {`say hey idiots, the site is broken`}
   end
   print "Received #{status} from act.watchdog.net\r"
