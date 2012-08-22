8ea53ee72b4d7ec6249a54ad3d5e6ed697db24cf
Vinicius Gomes <vvgomess@gmail.com>
2012-08-22 18:36:12 -0400

    Handling deadlocks for mailer process tracker

--- spec/models/mailer_process_tracker_spec.rb
+++ spec/models/mailer_process_tracker_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe MailerProcessTracker do
+  
+  context '.create' do
+    before do
+      MailerProcessTracker.delete_all
+      MailerProcessTracker.create
+    end
+
+    it "should create an entry if no mailer process tracker exists" do
+      MailerProcessTracker.count.should == 1
+      MailerProcessTracker.first.is_locked.should be_false
+    end
+
+    it "should not create a  new entry if there is a mailer process tracker already" do
+      MailerProcessTracker.create
+      MailerProcessTracker.count.should == 1
+    end
+  end
+  
+  it "should get the only tracker entry if one exists" do
+    MailerProcessTracker.delete_all
+    first = MailerProcessTracker.create
+
+    MailerProcessTracker.should_receive(:first).and_return first
+    MailerProcessTracker.in_transaction {}
+  end
+
   it "takes a lock on the tracking record" do
     record = MailerProcessTracker.new
     record.save!
@@ -9,6 +35,7 @@ describe MailerProcessTracker do
       MailerProcessTracker.first.is_locked.should be_true
     end
   end
+  
   it "releases the lock" do
     record = MailerProcessTracker.new
     record.save!
@@ -17,17 +44,55 @@ describe MailerProcessTracker do
     
     MailerProcessTracker.first.is_locked.should be_false
   end
-  it "aborts if no lock record is available" do
-    MailerProcessTracker.in_transaction do
-      raise "should not have got here!"
+
+  context "lock has already been taken and not deadlocked" do
+    
+    before do 
+      record = MailerProcessTracker.new(:is_locked => true)
+      record.save!
+      MailerProcessTracker.stub!(:put_to_sleep)
+    end 
+
+    it 'aborts if not deadlocked in the second check' do
+      MailerProcessTracker.in_transaction do
+        raise "should not have got here!"
+      end
+    end
+
+    it 'unlocks if deadlocked in the second check' do
+      MailerProcessTracker.any_instance.stub(:deadlocked?).and_return(false, true)
+      MailerProcessTracker.in_transaction {}
+      MailerProcessTracker.first.is_locked?.should be_false
     end
   end
-  it "aborts if lock has already been taken" do
-    record = MailerProcessTracker.new(:is_locked => true)
-    record.save!
-    
-    MailerProcessTracker.in_transaction do
-      raise "should not have got here!"
+
+  context 'when it is deadlock' do
+
+    subject { MailerProcessTracker.new is_locked: true }
+
+    it 'releases the lock in the first check' do
+      first = MailerProcessTracker.create
+      MailerProcessTracker.should_receive(:first).and_return first
+      MailerProcessTracker.any_instance.stub(:deadlocked?).and_return(true, false)
+      MailerProcessTracker.stub(:put_to_sleep)
+      MailerProcessTracker.in_transaction {}
+      first.is_locked.should be_false
     end
+
+  end
+
+  describe '#deadlocked?' do
+
+    subject { MailerProcessTracker.new is_locked: true }
+
+    it 'should return true when locked for more than 4 minutes' do  
+      subject.updated_at = (5).minutes.ago
+      subject.should be_deadlocked
+    end
+
+    it 'should be false when locked for less than 4 minutes' do
+      subject.should_not be_deadlocked
+    end
+
   end
 end
