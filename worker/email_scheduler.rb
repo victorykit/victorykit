de5e35f5147cc63a8de768c44979d235dc5ddaf2
Vinicius Gomes <vvgomess@gmail.com>
2012-08-22 18:36:12 -0400

    Handling deadlocks for mailer process tracker

--- worker/email_scheduler.rb
+++ worker/email_scheduler.rb
@@ -4,18 +4,24 @@ require 'petition_emailer'
 WEEK = 60*60*24*7
 
 class EmailScheduler
+  
   def self.schedule_email
-    max_emails_per_week = Member.count.to_f
-
     MailerProcessTracker.in_transaction do
       while 1
+        max_emails_per_week = Member.count.to_f
         last_email = Time.now
-        PetitionEmailer.send
+        send_email
         interval = WEEK/max_emails_per_week - (Time.now-last_email)
         sleep(interval) unless interval < 0
       end
     end
   end
+
+  def self.send_email
+    PetitionEmailer.send
+    MailerProcessTracker.update
+  end
 end
+
 
 EmailScheduler.schedule_email
