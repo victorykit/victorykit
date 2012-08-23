343c2af5bc2682d320f9ede057c8fa838fc691c9
Jim Arnold <jim@thoughtworks.com>
2012-08-23 16:44:40 -0400

    daemonized build_checker

--- script/check_build.rb
+++ script/check_build.rb
@@ -1,20 +1,47 @@
 #!/usr/bin/env ruby
 
 require File.expand_path('../../config/application', __FILE__)
-
+require 'daemons'
 require 'selenium/webdriver'
 require 'rails_on_fire'
 require 'build_checker'
 
-$rof_user = ENV['ROF_USER']
-$rof_password = ENV['ROF_PASSWORD']
-
-raise "You need to set ROF_USER and ROF_PASSWORD env vars" if !$rof_user || !$rof_password
-
-begin
-  d = Selenium::WebDriver.for :chrome
-  rof = RailsOnFire.new d
-  BuildChecker.new(rof, VictoryKitChat).run
-ensure
-  d.quit
+def start
+  demonize
+  log("build checker is now started")
 end
+
+def stop
+  demonize
+  log "build checker is now stopped"
+end
+
+def demonize
+  Daemons.run_proc('build_checker', :dir_mode => :normal, :dir => '/var/tmp/', :monitor => false) do
+
+    rof_user = ENV['ROF_USER']
+    rof_password = ENV['ROF_PASSWORD']
+    
+    raise "You need to set ROF_USER and ROF_PASSWORD env vars" if !rof_user || !rof_password
+
+    begin
+      d = Selenium::WebDriver.for :chrome
+      rof = RailsOnFire.new d, rof_user, rof_password
+      BuildChecker.new(rof, VictoryKitChat).run
+    ensure
+      d.quit
+    end
+  end
+end
+
+case ARGV[0]
+  when 'start'
+    start
+  when 'stop'
+    stop
+  else
+    puts "usage: check_build start|stop"
+end
+
+
+
