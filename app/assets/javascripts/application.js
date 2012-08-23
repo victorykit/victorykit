1fb87dcf75fc5f190f4b2d7a1d0eb8ff4b4a2062
Brian Guthrie <btguthrie@gmail.com>
2012-08-23 18:04:59 -0400

    Added a countdown timer experiment to the share modal.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require jquery.flot
 //= require jquery.mailcheck
 //= require jquery.counter
+//= require jquery.countdown
 //= require petition
 //= require global
 //= require petition_editor
