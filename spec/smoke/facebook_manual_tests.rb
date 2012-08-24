81cd3735130974c184f77ea3580dd306af965b38
Martin Andersen <manderdev@gmail.com>
2012-08-24 10:43:19 -0400

    minor updates to manual facebook share test script

--- spec/smoke/facebook_manual_tests.rb
+++ spec/smoke/facebook_manual_tests.rb
@@ -6,28 +6,29 @@ describe "share on facebook using facebook_popup" do
   end
 
   #assumptions:
-  #test against testserver (will not work in all cases on localhost, e.g. where fb uses callbacks)
-  #fb account for victorkfb
+  #test against testserver (fb callbacks won't hit localhost). testserver url must be registered as fb app
+  #fb account exists: victorkfb. todo: fb test accounts -> http://developers.facebook.com/docs/test_users/
+
+  #note:
+  #we need more than one of the spinnable options, otherwise they aren't tracked as experiments
 
   #----------
   #sign in to victorykit as admin
   #create a petition: i like turtles
   
-  #note: we need more than one of the spinnable options, otherwise they aren't tracked as experiments
-
-  #click 'Customize Facebook Title'  
-  #enter an alternate title: turtles are awesome
-  #enter an alternate title: turtles are really awesome
+  #click: 'Customize Facebook Title'
+  #enter an alternate title: fb alt title a
+  #enter an alternate title: fb alt title b
   
-  #click 'Customize Facebook Description'  
-  #enter an alternate description: because they have shells
-  #enter an alternate description: because they have nice shells
+  #click: 'Customize Facebook Description'
+  #enter an alternate description: fb alt desc a
+  #enter an alternate description: fb alt desc b
 
-  #click 'Image Link'
+  #click: 'Image Link'
   #enter an image url: www.maniacworld.com/i-lik-turtles.jpg
   #enter an image url: http://i3.kym-cdn.com/photos/images/original/000/181/192/tumblr_ls86k4MTEo1qgs1ido1_400.gif
 
-  #click 'Create Petition' button
+  #click: 'Create Petition' button
   #note the id of the petition for later use
 
   #click: log out
@@ -49,8 +50,14 @@ describe "share on facebook using facebook_popup" do
   #sign in to victorykit as admin
   #go to /admin/experiments?f=petitions
 
-  #check spins against petition #{id} facebook title: 0
+  #check spins against petition #{id} facebook title: 1
   #check wins against petition #{id} facebook title: 0
+
+  #check spins against petition #{id} facebook image: 1
+  #check wins against petition #{id} facebook image: 0
+
+  #go to /admin/experiments?f=experiments
+  #check wins against facebook sharing options: facebook_popup: 0
 
   #----------
   #go to facebook (logged in as victorkfb)
@@ -59,20 +66,24 @@ describe "share on facebook using facebook_popup" do
   #verify that petition description is alternate description: because they have shells
 
   #----------
-  #sign in to victorykit.com as admin
-  #go to /admin/experiments
-
-  #check spins against petition #{id} facebook title: 1
-  #check wins against petition #{id} facebook title: 0
-  #check wins against facebook sharing options: facebook_popup: 0
-
-  #----------
   #copy petition link from facebook entry
+  #petition link should have a referral param of some sort: share_ref=#{member_hash}
   #open link in new browser instance
   #sign petition as another user: victoriakfb??
 
-  #check wins against facebook sharing options: facebook_popup: 1
-  #check spins against petition #{id} facebook title: 1
+  #----------
+  #sign in to victorykit.com as admin
+  #go to /admin/experiments
+
+  #note:
+  #spins count of 2 is due to current sub-optimal implementation which spins on signing instead of sharing
+
+  #check spins against petition #{id} facebook title: 2
   #check wins against petition #{id} facebook title: 1
+
+  #check spins against petition #{id} facebook image: 2
+  #check wins against petition #{id} facebook image: 1
+
+  #check wins against facebook sharing options: facebook_popup: 1
 
 end
