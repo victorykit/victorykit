6c28876b25bdd30973964010771bb31371972fb9
Martin Andersen <manderdev@gmail.com>
2012-08-27 21:18:04 -0600

    facebook integration for spec test - now works, round trip

--- spec/smoke/facebook_title_experiments.rb
+++ spec/smoke/facebook_title_experiments.rb
@@ -6,8 +6,6 @@ describe 'creating a facebook title experiment' do
 
   it 'awards a win against the facebook title when facebook user signs' do
 
-    pending 'work in progress [martin/vini]'
-
     petition = create_a_featured_petition({
       title: 'Multiple facebook titles!',
       description: 'You betcha',
@@ -20,59 +18,35 @@ describe 'creating a facebook title experiment' do
       'display countdown to share' => 'false' 
     })
 
-    create_facebook_test_user
-    
+    fb_test_user = create_facebook_test_user
+
     go_to petition_path(petition)
     sign_petition
-    share_petition_on_facebook
-    
+    expected_shared_link = expected_facebook_share_link petition, current_member
+    share_petition_on_facebook fb_test_user, :share
+
     experiment = facebook_experiment_results_for petition
     experiment.spins.should eq 1
     experiment.wins.should eq 0
     
-    link_from_fb = facebook_referral_link petition, current_member
     delete_member_cookie
-    
-    go_to link_from_fb
+    go_to_facebook
+    click_shared_link expected_shared_link
     sign_petition
     
     experiment = facebook_experiment_results_for petition
     experiment.spins.should eq 1
     experiment.wins.should eq 1
   end
-
-  def share_petition_on_facebook
-    click(:id => 'the-one-in-the-modal')
-    find_facebook_share_popup
+  
+  def click_shared_link expected_shared_link
+    actual_shared_link = element(link: "a link").attribute "href"
+    Rails.logger.debug "actual shared link: #{actual_shared_link}"
+    actual_shared_link.match(CGI.escape(expected_shared_link)).should_not be_nil
+    go_to expected_shared_link
   end
   
-  def find_facebook_share_popup
-    #type('victorkfb@gmail.com').into(:id => 'email')
-    #home = $driver.current_window_handle;
-    #$driver.window_handles
-    # foreach (var window in _driver.WindowHandles)
-    #  {
-    #      if (_driver.SwitchTo().Window(window).Title.Contains("Search"))
-    #      {
-    #          _driver.SwitchTo().Frame("resultsFrame");
-    #         PageHelper.Country.Click();
-    #          break;
-    #      }
-    #  }
-    # _driver.SwitchTo().Window(home);
-  end
-  
-  def facebook_experiment_results_for petition
-    as_admin do
-      go_to 'admin/experiments?f=petitions'
-      table = element(xpath: "//table[@id = 'petition #{petition.id} facebook title']")
-      spins = table.find_element(xpath: "tbody/tr/td[@class='spins']").text.to_i
-      wins = table.find_element(xpath: "tbody/tr/td[@class='wins']").text.to_i
-      return OpenStruct.new(spins: spins, wins: wins)
-    end
-  end
-  
-  def facebook_referral_link petition, member
+  def expected_facebook_share_link petition, member
     "#{petition_path(petition)}?share_ref=#{member.to_hash}"
   end
 end
--- spec/smoke_spec_helper.rb
+++ spec/smoke_spec_helper.rb
@@ -225,7 +225,7 @@ def current_member
   Member.find_by_hash(cookie[:value]) or raise "member_id cookie value did not unhash"
 end
 
-def create_facebook_test_user user_name="victor", installed=false  
+def create_facebook_test_user user_name="victor", installed=false, login=true
   access_token = fetch_facebook_access_token
   uri = 
     "https://graph.facebook.com/#{facebook_app_id}/accounts/test-users?"\
@@ -233,7 +233,11 @@ def create_facebook_test_user user_name="victor", installed=false
     "&name=#{user_name}"\
     "&locale=en_US&permissions=read_stream&method=post"\
     "&access_token=#{access_token}"    
-  JSON.parse(URI.parse(URI.encode(uri)).read)
+  Rails.logger.debug "creating fb test user: #{uri}"
+  test_user = JSON.parse(URI.parse(URI.encode(uri)).read)
+  Rails.logger.debug "test user: #{test_user}"
+  login_at_facebook test_user if login
+  test_user
 end
 
 def fetch_facebook_access_token
@@ -242,7 +246,29 @@ def fetch_facebook_access_token
     "?client_id=#{facebook_app_id}"\
     "&client_secret=#{facebook_secret}"\
     "&grant_type=client_credentials"
-  URI.parse(uri).read.match(/^access_token=(.*)/)[1]
+  Rails.logger.debug "fetching fb access token: #{uri}"
+  token = URI.parse(uri).read.match(/^access_token=(.*)/)[1]
+  Rails.logger.debug "access token: #{token}"
+  token
+end
+
+def login_at_facebook test_user
+  # you can either do it this way, where you call it twice
+  # (has to be twice! waiting 1-30 seconds won't do it. no sir. twice is the trick.)...
+  go_to_external test_user["login_url"]
+  go_to_external test_user["login_url"]
+
+  # ...or you can do it this way (kept as backup in case of failure of the other),
+  # which also works but is a bit slower:
+  # go_to_external "http://www.facebook.com"
+  # element(id: "email")
+  # type(test_user["email"]).into(id: "email")
+  # type(test_user["password"]).into(id: "pass")
+  # click(id: "loginbutton")
+end
+
+def go_to_facebook
+  go_to_external 'http://www.facebook.com'
 end
 
 def facebook_app_id
@@ -251,4 +277,25 @@ end
 
 def facebook_secret
   ENV['FACEBOOK_SECRET'] || raise("Missing FACEBOOK_SECRET setting in environment variables")
+end
+
+def share_petition_on_facebook fb_test_user, share_mode
+  if (share_mode == :share)
+    click(:id => 'the-one-in-the-modal')
+    $driver.switch_to.window $driver.window_handles.last
+    click(:name => 'share')
+    $driver.switch_to.window $driver.window_handles.first
+  else
+    raise "FB sharing mode #{share_mode} not yet supported in smoke specs. Heave away."
+  end
+end
+
+def facebook_experiment_results_for petition
+  as_admin do
+    go_to 'admin/experiments?f=petitions'
+    table = element(xpath: "//table[@id = 'petition #{petition.id} facebook title']")
+    spins = table.find_element(xpath: "tbody/tr/td[@class='spins']").text.to_i
+    wins = table.find_element(xpath: "tbody/tr/td[@class='wins']").text.to_i
+    return OpenStruct.new(spins: spins, wins: wins)
+  end
 end
