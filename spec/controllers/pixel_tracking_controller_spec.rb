550216b8c5ed9169b16cd116cd60c605e9918ba0
rachel kammerer <rlkammerer@gmail.com>
2012-08-28 15:03:15 -0400

    fix flakey test with rspec

--- spec/controllers/pixel_tracking_controller_spec.rb
+++ spec/controllers/pixel_tracking_controller_spec.rb
@@ -18,7 +18,7 @@ describe PixelTrackingController do
       onedayago = 1.day.ago
       email = create :sent_email, :opened_at => onedayago
       get :new, :n => email.to_hash
-      email.reload.opened_at.should == onedayago
+      email.reload.opened_at.should eql(onedayago)
     end
 
     it "should not do anything if hash is invalid" do
