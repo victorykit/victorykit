cb74a182b09a7e93da7f11ca15fd7522506a1d99
Vinicius Gomes <vvgomess@gmail.com>
2012-08-29 19:03:26 -0400

    adding specs for missing cases

--- app/models/petition_statistics.rb
+++ app/models/petition_statistics.rb
@@ -4,7 +4,6 @@ class PetitionStatistics
     @petition = petition
     @local_stats = local_stats
   end
-  
   def p; @petition end
   def email_count; @local_stats.sent_emails end
   def opened_emails_count; @local_stats.opened_emails end
@@ -14,14 +13,7 @@ class PetitionStatistics
   def likes_count; @analytics_data.nil? ? 0 : @analytics_data.likes.to_i end
   def hit_count; @analytics_data.nil? ? 0 : @analytics_data.unique_pageviews.to_i end
   def new_member_count; @local_stats.new_members end
-  def unsubscribe_count
-    @local_stats.unsubscribes
-  end
-  
-  def divide_safe(numerator, denominator)
-    denominator.nonzero? ? numerator / denominator.to_f : 0.0
-  end
-
+  def unsubscribe_count; @local_stats.unsubscribes end
   def open_rate; divide_safe(opened_emails_count, email_count) end
   def clicked_rate; divide_safe(clicked_emails_count, email_count) end
   def sign_rate; divide_safe(email_signature_count, email_count) end
@@ -29,7 +21,9 @@ class PetitionStatistics
   def hit_rate; divide_safe(hit_count, email_count) end
   def new_rate; divide_safe(new_member_count, email_count) end
   def unsub_rate; divide_safe(unsubscribe_count, email_count) end
-
   def petition_title; p.title end
   def petition_created_at; p.created_at end
+  def divide_safe(numerator, denominator)
+    denominator.nonzero? ? numerator / denominator.to_f : 0.0
+  end
 end
--- spec/models/petition_statistics_spec.rb
+++ spec/models/petition_statistics_spec.rb
@@ -1,23 +1,28 @@
 require 'spec_helper'
 
 describe PetitionStatistics do
-  let(:google_data) { OpenStruct.new(:unique_pageviews=>"100") }
-  let(:local_data) { OpenStruct.new(:signatures => 1, :new_members => 0) }
-  let(:petition) { create(:petition_with_signatures, signature_count: 1) }
+  subject { PetitionStatistics.new(petition, google_data, local_data) }
   
-  describe "when statistics are available" do
-    subject { PetitionStatistics.new(petition, google_data, local_data) }
-    
-    its(:hit_count) { should == 100 }
-    its(:signature_count) { should ==  1 }
-    its(:new_member_count) { should ==  0 }
+  let(:local_data) { OpenStruct.new(signatures: 1, new_members: 0) }
+  let(:petition) { build(:petition_with_signatures, signature_count: 1) }
+  let(:google_data) { OpenStruct.new(unique_pageviews: '100') }
+  
+  describe 'when statistics are available' do
+    its(:hit_count) { should eq 100 }
+    its(:signature_count) { should eq  1 }
+    its(:new_member_count) { should eq  0 }
   end
 
-  describe "when statistics are unavailable" do        
-    subject { PetitionStatistics.new(petition, nil, local_data) }
+  describe 'when statistics are unavailable' do
+    let(:google_data) { nil }
     
-    its(:hit_count) { should == 0 }
-    its(:signature_count) { should ==  1 }
-    its(:new_member_count) { should ==  0 }
+    its(:hit_count) { should eq 0 }
+    its(:signature_count) { should eq  1 }
+    its(:new_member_count) { should eq  0 }
+  end
+
+  describe '#divide_safe' do
+    specify { subject.divide_safe(10, 2).should eq 5 }
+    specify { subject.divide_safe(10, 0).should eq 0 }
   end
 end
