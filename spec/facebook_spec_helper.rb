cab6f0f050fb553236f29336a5162de22f4107b7
Martin Andersen <manderdev@gmail.com>
2012-08-29 23:23:14 -0600

    added fb end-to-end testing for fb request; further developed test for fb popup share

--- spec/facebook_spec_helper.rb
+++ spec/facebook_spec_helper.rb
@@ -1,28 +1,77 @@
-def create_facebook_test_user user_name="victor", installed=false, login=true
+def create_facebook_test_user user_name="victor", login=true
   access_token = fetch_facebook_access_token
   uri = 
     "https://graph.facebook.com/#{facebook_app_id}/accounts/test-users?"\
-    "installed=#{installed}"\
+    "installed=true"\
     "&name=#{user_name}"\
     "&locale=en_US&permissions=read_stream&method=post"\
     "&access_token=#{access_token}"    
-  Rails.logger.debug "creating fb test user: #{uri}"
+  log "creating fb test user: #{uri}"
   test_user = JSON.parse(URI.parse(URI.encode(uri)).read)
-  Rails.logger.debug "test user: #{test_user}"
+  log "test user: #{test_user}"
   login_at_facebook test_user if login
   test_user
 end
 
+def log message
+  Rails.logger.debug "FB: #{message}"
+end
+
+def facebook_friend user_a, user_b
+  id_a = user_a["id"]
+  id_b = user_b["id"]
+  token_a = user_a["access_token"]
+  token_b = user_b["access_token"]
+
+  uri = "https://graph.facebook.com/#{id_a}/friends/#{id_b}?method=post&access_token=#{token_a}"
+  log "friend request: posting from user #{id_a} to #{id_b}"
+  log uri
+  result = URI.parse(URI.encode(uri)).read
+  log result
+
+  uri = "https://graph.facebook.com/#{id_b}/friends/#{id_a}?method=post&access_token=#{token_b}"
+  log "friend request: accepting by user #{id_b} in response to #{id_a}"
+  log uri
+  result = URI.parse(URI.encode(uri)).read
+  log result
+end
+
+def delete_fb_test_user test_user
+  delete_fb_test_user_by_id test_user["id"]
+end
+
+def delete_fb_test_user_by_id id
+  uri = "https://graph.facebook.com/#{id}?method=delete&access_token=#{fetch_facebook_access_token}"
+  URI.parse(URI.encode(uri)).read
+end
+
+def delete_all_fb_test_users
+  uri = "https://graph.facebook.com/#{facebook_app_id}/accounts/test-users?access_token=#{fetch_facebook_access_token}"
+  while uri
+    uri = delete_page_of_fb_test_users uri
+  end
+end
+
+def delete_page_of_fb_test_users uri
+  j = JSON.parse(URI.parse(URI.encode(uri)).read)
+  j["data"].each do |data|
+     delete_fb_test_user_by_id data["id"]
+  end
+  j["paging"] ? j["paging"]["next"] : nil
+end
+
 def fetch_facebook_access_token
-  uri = 
-    "https://graph.facebook.com/oauth/access_token"\
-    "?client_id=#{facebook_app_id}"\
-    "&client_secret=#{facebook_secret}"\
-    "&grant_type=client_credentials"
-  Rails.logger.debug "fetching fb access token: #{uri}"
-  token = URI.parse(uri).read.match(/^access_token=(.*)/)[1]
-  Rails.logger.debug "access token: #{token}"
-  token
+  if not @fb_access_token
+    uri =
+      "https://graph.facebook.com/oauth/access_token"\
+      "?client_id=#{facebook_app_id}"\
+      "&client_secret=#{facebook_secret}"\
+      "&grant_type=client_credentials"
+    log "fetching fb access token: #{uri}"
+    @fb_access_token = URI.parse(uri).read.match(/^access_token=(.*)/)[1]
+    log "access token: #{@fb_access_token}"
+  end
+  @fb_access_token
 end
 
 def facebook_app_id
@@ -48,48 +97,52 @@ def login_at_facebook test_user
   # click(id: "loginbutton")
 end
 
-def go_to_facebook
-  go_to_external 'http://www.facebook.com'
+def go_to_facebook path=nil
+  uri = "http://www.facebook.com#{path}"
+  go_to_external uri
 end
 
 def share_petition_on_facebook fb_test_user, share_mode
+  click(:id => 'the-one-in-the-modal')
   if (share_mode == :share)
-    click(:id => 'the-one-in-the-modal')
     $driver.switch_to.window $driver.window_handles.last
     click(:name => 'share')
+    $driver.switch_to.window $driver.window_handles.first
+  elsif (share_mode) == :request
+    switch_to_frame(:class => "FB_UI_Dialog")
+    checkbox = wait.until { element(class: "checkbox") }
+    checkbox.click
+    element(name: "ok_clicked").click
     $driver.switch_to.window $driver.window_handles.first
   else
     raise "FB sharing mode #{share_mode} not yet supported in smoke specs. Heave away."
   end
 end
 
-def at_petition_experiments
+def as_admin_at_petition_experiments
   as_admin do
     go_to 'admin/experiments?f=petitions'
       yield
   end
 end
 
-def find_experiment_results experiment_name
-  table = element(xpath: "//table[@id = '#{experiment_name}']")
-  spins = table.find_element(xpath: "tbody/tr/td[@class='spins']").text.to_i
-  wins = table.find_element(xpath: "tbody/tr/td[@class='wins']").text.to_i
-  return OpenStruct.new(spins: spins, wins: wins)
+def click_shared_link expected_shared_link_match
+  verify_and_click_link "a link", CGI.escape(expected_link_match)
 end
 
-def assert_petition_experiment_results experiment_name, spins, wins
-  experiment = find_experiment_results experiment_name
-  experiment.spins.should eq spins
-  experiment.wins.should eq wins
+def click_request_link
+  verify_and_click_link "a request", CGI.escape("http://apps.facebook.com/victorykit_dev/?fb_source=notification&request_ids=")
 end
 
-def click_shared_link expected_shared_link
-  actual_shared_link = element(link: "a link").attribute "href"
-  Rails.logger.debug "actual shared link: #{actual_shared_link}"
-  actual_shared_link.match(CGI.escape(expected_shared_link)).should_not be_nil
-  go_to expected_shared_link
+def verify_and_click_link link_text, link_match
+  actual_link = element(link: link_text).attribute "href"
+  log "found link: #{actual_link}"
+  log "verifying link against: #{link_match}"
+  CGI.escape(actual_link).match(link_match).should_not be_nil
+  go_to actual_link
 end
 
-def expected_facebook_share_link petition, member
-  "#{petition_path(petition)}?share_ref=#{member.to_hash}"
+def switch_to_frame locator
+  frame_id = $driver.find_element(locator)["id"]
+  $driver.switch_to.frame frame_id
 end
--- spec/smoke/facebook_title_experiments.rb
+++ spec/smoke/facebook_title_experiments.rb
@@ -1,9 +1,12 @@
 require 'smoke_spec_helper'
 require 'facebook_spec_helper'
 
-describe 'creating a facebook title experiment' do
+describe 'facebook sharing experiments' do
 
-  it 'awards a win against the facebook title when facebook user signs' do
+  # facebook_popup
+  it 'registers wins when facebook_popup shared link yields signature' do
+
+    pending 'need to stabilize'
 
     petition = create_a_featured_petition({
       title: 'Multiple facebook titles!',
@@ -18,27 +21,97 @@ describe 'creating a facebook title experiment' do
       'display countdown to share' => 'false' 
     })
 
-    fb_test_user = create_facebook_test_user
+    begin
+      fb_victor = create_facebook_test_user
 
-    go_to petition_path(petition)
-    sign_petition
-    expected_shared_link = expected_facebook_share_link petition, current_member
-    share_petition_on_facebook fb_test_user, :share
+      go_to petition_path(petition)
+      sign_petition
+      share_petition_on_facebook fb_victor, :share
 
-    at_petition_experiments do
-      assert_petition_experiment_results "petition #{petition.id} facebook title", 1, 0
-      assert_petition_experiment_results "petition #{petition.id} facebook image", 1, 0
+      #need to capture the expected link before clearing the current_member cookie
+      expected_shared_link = "#{petition_path(petition)}?share_ref=#{current_member.to_hash}"
+      delete_member_cookie
+
+      as_admin_at_petition_experiments do
+        assert_petition_experiment_results "petition #{petition.id} facebook title", 1, 0
+        assert_petition_experiment_results "petition #{petition.id} facebook image", 1, 0
+      end
+
+      # cheat: going back to fb as fb_victor to save time creating another user and logging in again
+      go_to_facebook
+      click_shared_link expected_shared_link
+      sign_petition
+
+      as_admin_at_petition_experiments do
+        assert_petition_experiment_results "petition #{petition.id} facebook title", 1, 1
+        assert_petition_experiment_results "petition #{petition.id} facebook image", 1, 1
+      end
+    ensure
+      #todo: better to use something self-scoping like a yield block, but need access to fb_victor in block, so...
+      delete_fb_test_user fb_victor
     end
-    
-    delete_member_cookie
-    go_to_facebook
-    click_shared_link expected_shared_link
-    sign_petition
+  end
 
-    at_petition_experiments do
-      assert_petition_experiment_results "petition #{petition.id} facebook title", 1, 1
-      assert_petition_experiment_results "petition #{petition.id} facebook image", 1, 1
+  #facebook_request
+  it 'registers wins when facebook_request shared link yields signature' do
+
+    pending 'need to stabilize'
+
+    petition = create_a_featured_petition({
+      title: 'Multiple facebook titles!',
+      description: 'You betcha',
+      facebook_titles: ['FB Title A', 'FB Title B'],
+      images: ['placekitten.com/g/200/200','placekitten.com/g/200/220']
+    })
+
+    force_result({
+      'facebook sharing options' => 'facebook_request',
+      'after share view 2' => 'button_is_most_effective_tool',
+      'display countdown to share' => 'false'
+    })
+
+    begin
+      fb_victor = create_facebook_test_user "victor"
+      fb_vincent = create_facebook_test_user "vincent", false
+      facebook_friend fb_victor, fb_vincent
+
+      go_to petition_path(petition)
+      sign_petition
+      expected_shared_link = "#{petition_path(petition)}?share_ref=#{current_member.to_hash}"
+      share_petition_on_facebook fb_victor, :request
+
+      as_admin_at_petition_experiments do
+        assert_petition_experiment_results "petition #{petition.id} facebook title", 1, 0
+        assert_petition_experiment_results "petition #{petition.id} facebook image", 1, 0
+      end
+
+      delete_member_cookie
+      login_at_facebook fb_vincent
+      go_to_facebook "/notifications"
+      click_request_link
+
+      switch_to_frame(:class => "smart_sizing_iframe")
+      element(class: "mobile_signup_button").click
+      sign_petition "Vincent", "leTest", fb_vincent["email"]
+
+      as_admin_at_petition_experiments do
+        assert_petition_experiment_results "petition #{petition.id} facebook title", 2, 1
+        assert_petition_experiment_results "petition #{petition.id} facebook image", 2, 1
+      end
+    ensure
+      delete_fb_test_user fb_victor
+      delete_fb_test_user fb_vincent
     end
   end
   
+  # facebook_wall
+  it 'registers win for facebook_wall' do
+    pending 'implement test'
+  end
+end
+
+#todo: temporary while sorting out fb testing and cleanup
+describe 'delete all fb test users' do
+  pending 'run manually when needed'
+  delete_all_fb_test_users
 end
