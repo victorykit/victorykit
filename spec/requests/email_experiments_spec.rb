2ade2d27210e2398a98869b9bb29062cd19584ef
Vinicius Gomes <vvgomess@gmail.com>
2012-08-30 10:52:20 -0400

    making the spec more accurate

--- spec/requests/email_experiments_spec.rb
+++ spec/requests/email_experiments_spec.rb
@@ -4,30 +4,37 @@ describe 'email experiments' do
     let(:member) { create :member }
     let(:user  ) { create :admin_user }
 
-    #FIXME: split in multiple examples, like:
-    # - should allow multiple subjects
-    # - should send email with the hashed link
-    # - should compute win after signin
-
-    it 'should win after signature from email', :js => true, :driver => :webkit do
+    it 'should win after signature from email', js: true, driver: :webkit  do
       login user.email, user.password do
-        create_petition(subjects: ['tutles 1', 'tutles 2'])
-        
-        petition = Petition.last
-        
-        visit on_demand_email_path(petition, member)
-        link = find_link('Please, click here to sign now!')[:href]
-        hash = link.scan(/n=(.*)$/).join
 
-        results = email_experiment_results_for petition
-        results[:spins].should eq 1
-        results[:wins ].should eq 0
+        # create two petitions
+        petitions = 2.times.map do
+          create_petition(subjects: ['tutles 1', 'tutles 2'])
+        end
 
-        sign petition, { n: hash }
+        # send emails for them
+        hashes = petitions.reduce({}) do |result, petition|
+          visit on_demand_email_path(petition, member)
+          link = find_link('Please, click here to sign now!')[:href]
+          hash = link.scan(/n=(.*)$/).join
+          result[petition] = hash ; result
+        end
 
-        results = email_experiment_results_for petition
-        results[:spins].should eq 1
-        results[:wins ].should eq 1
+        # check results
+        petitions.each do |petition|
+          results = email_experiment_results_for petition
+          results[:spins].should eq 1
+          results[:wins ].should eq 0
+        end
+
+        # sign and check again
+        petitions.each do |petition|
+          sign petition, { n: hashes[petition] }
+          results = email_experiment_results_for petition
+          results[:spins].should eq 1
+          results[:wins ].should eq 1
+        end
+
       end
     end
 
