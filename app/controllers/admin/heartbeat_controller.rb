50878953166a4d7a6fe0d3597c551746670916e8
Martin Andersen <manderdev@gmail.com>
2012-09-13 16:50:39 -0600

    rejigged admin heartbeat to include resque failures as application failure; added failure condition on max q size >= 100; extracted status logic to model classes; changed 'Status: OK' to 'Application Status: OK' to avoid future false positives (updating new relic subsequently)

--- app/controllers/admin/heartbeat_controller.rb
+++ app/controllers/admin/heartbeat_controller.rb
@@ -1,36 +1,15 @@
+require 'admin/application_status'
+
 class Admin::HeartbeatController < ApplicationController
 
   newrelic_ignore
-  helper_method :display_content?
+  helper_method :display_content?, :email_status_style, :email_status_text, :signature_status_style, :signature_status_text
 
   def index
-    last_email = SentEmail.last
-    @email_threshold = ENV['VK_HEARTBEAT_SENT_EMAIL'].try(:to_i) || 5
-    @last_email_timestamp = last_email.created_at
-    @email_working = @last_email_timestamp > @email_threshold.minutes.ago
-    if not @email_working
-      Rails.logger.error "Heartbeat: emails inactive since #{@last_email_timestamp}"
-    end
-
-    # Failing on a shortage of signatures turned out to be a bit overzealous, particularly in the middle of the night.
-    # Instead, we'll keep it on the page so it's visible for manual checks but not fail over it. We can redefine it as
-    # a ratio of signatures per page hit to take late night and holiday fluctuations into account.
-    last_signature = Signature.last
-    @signature_threshold = ENV['VK_HEARTBEAT_SIGNATURE'].try(:to_i) || 60
-    @last_signature_timestamp = last_signature.created_at
-    @signature_working = @last_signature_timestamp > @signature_threshold.minutes.ago
-    if not @signature_working
-      Rails.logger.warn "Heartbeat: signatures inactive since #{@last_signature_timestamp}"
-    end
-
-    @email_failing_style = "failing"
-    @signature_failing_style = "warning"
-
-    @overall_working = @email_working
-    @overall_status = "FAILING"
-    @overall_status = "OK" if @overall_working
-    @resque_working = Resque.info[:workers] > 0
-    @resque_stats = Resque.info
+    @status = ApplicationStatus.new
+    @status[:email] = EmailStatus.new(true)
+    @status[:signature] = SignatureStatus.new(false)
+    @status[:resque] = ResqueStatus.new(true)
 
     @emails_sent_past_week = SentEmail.where("created_at > ?", 1.week.ago).count
     @emailable_members = Member.count - Unsubscribe.count
---
+++ app/models/admin/application_status.rb
@@ -0,0 +1,116 @@
+class ApplicationStatus
+
+  def initialize
+    @items = {}
+  end
+
+  def [] k
+    @items[k]
+  end
+
+  def []= k,v
+    @items[k] = v
+  end
+
+  def ok?
+    @ok = @items.values.find_all { |status| status.fails_app? } .all? { |status| status.ok? }
+  end
+
+  # ATTENTION: newrelic expects to see "Status: OK" on the page, otherwise it sends out alerts. 
+  # So if you change this here, make sure you also update newrelic. Especially if you change it late at night :)
+  def text
+    ok? ? "OK" : "FAILING"
+  end
+
+  # Style for overall application status should apply to both label and value because newrelic doesn't seem to like spans in its substring check.
+  def style
+    ok? ? "ok" : "failing"
+  end
+
+end
+
+class ItemStatus
+
+  def initialize fails_app, down_term="FAILING"
+    @fails_app = fails_app
+    @down_term = down_term
+  end
+
+  def fails_app?
+    @fails_app
+  end
+
+  def ok?
+    @ok
+  end
+
+  def style
+    @ok ? "ok" : (@fails_app ? "failing" : "warning")
+  end
+
+  def text
+    @ok ? "OK" : @down_term
+  end
+
+end
+
+class EmailStatus < ItemStatus
+  attr_accessor :threshold, :last_timestamp, :working
+
+  def initialize fails_app
+    super fails_app, "INACTIVE"
+    check
+  end
+
+  def check
+    @threshold = ENV['VK_HEARTBEAT_SENT_EMAIL'].try(:to_i) || 5
+    @last_timestamp = SentEmail.last.created_at
+    @ok = @last_timestamp > @threshold.minutes.ago
+    if not @working
+      Rails.logger.error "Heartbeat: emails inactive since #{@last_timestamp}"
+    end
+    @ok
+  end
+
+end
+
+class SignatureStatus < ItemStatus
+  attr_accessor :threshold, :last_timestamp, :working
+
+  def initialize fails_app
+    super fails_app, "INACTIVE"
+  end
+
+  def check
+    @threshold = ENV['VK_HEARTBEAT_SIGNATURE'].try(:to_i) || 60
+    # Failing on a shortage of signatures turned out to be a bit overzealous, particularly in the middle of the night.
+    # Instead, we'll keep it on the page so it's visible for manual checks but not fail over it. We can redefine it as
+    # a ratio of signatures per page hit to take late night and holiday fluctuations into account.
+    @last_timestamp = Signature.last.created_at
+    @ok = @last_timestamp > @threshold.minutes.ago
+    if not @working
+      Rails.logger.warn "Heartbeat: signatures inactive since #{@last_timestamp}"
+    end
+    @ok
+  end
+
+end
+
+class ResqueStatus < ItemStatus
+  attr_accessor :ok, :stats, :max_q
+
+  def initialize fails_app
+    super fails_app
+    check
+  end
+
+  def check
+    @stats = Resque.info
+    @ok = Resque.info[:workers] > 0 && max_q <= 100
+  end
+
+  def max_q
+    Resque.queues.max_by { |queue| Resque.size(queue) }
+  end
+
+end
