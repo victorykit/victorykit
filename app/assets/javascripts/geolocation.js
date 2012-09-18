023d979a19dee0f97cf91f58a7e7e2f76e3e1a95
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-09-18 16:55:46 -0400

    javascript logic to display/hide/load location info

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -3,6 +3,7 @@ var geolocation = (function() {
 
   function hideAll() {
     $('#states, #countries').addClass('hidden');
+    $('#location-details').val('');
   }
 
   function showStates() {
@@ -16,24 +17,34 @@ var geolocation = (function() {
   }
 
   function updateStates() {
-    var states =  $('#states .chzn-select').val();
-    var val = $.map(states, function(s){ return 'us/'+s; }).join(',');
-    $('#petition_location_us').val(val);
+    $('#location-details').val($('#states .chzn-select').val().join(','));
   }
 
   function updateCountries() {
-    var countries = $('#countries .chzn-select').val();
-    var val = $.map(countries, function(c){ return 'non-us/'+c; }).join(',');
-    $('#petition_location_non-us').val(val);
+    $('#location-details').val($('#countries .chzn-select').val().join(','));
+  }
+
+  function rebuildSelections() {
+    var loc = $('#location-options input[checked="checked"]').val();
+    var div = {'us': '#states', 'non-us': '#countries'}[loc];
+    if(!div) return;
+    var sel = ' .chzn-select';
+    var details = $('#location-details').val().split(',');
+    $(details).each(function(i, e) {
+      $(div+sel+' option[value="'+e+'"]').attr('selected', true);
+      $(div+sel).trigger('liszt:updated');
+    });
+    $(div).removeClass('hidden');
   }
 
   ns.initialize = function() {
     $('.chzn-select').chosen();
-    $('#petition_location_us').click(showStates);
-    $('#petition_location_non-us').click(showCountries);
-    $('#petition_location_all').click(hideAll);
+    $('#petition_location_type_us').click(showStates);
+    $('#petition_location_type_non-us').click(showCountries);
+    $('#petition_location_type_all').click(hideAll);
     $('#states .chzn-select').chosen().change(updateStates);
     $('#countries .chzn-select').chosen().change(updateCountries);
+    rebuildSelections();
   };
 
   return ns;
