bac0a39716282b04f2ed004d11f431c01f8f9f70
Brian Guthrie <btguthrie@gmail.com>
2012-09-27 16:58:20 -0400

    emergency fix.

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,5 +1,5 @@
 if base_name = ENV["STATSD_HOSTNAME"]
-  hostname_and_port, namespace = basename.split("@")
+  hostname_and_port, namespace = base_name.split("@")
   hostname, port = hostname_and_port.split(":")
 end
 
