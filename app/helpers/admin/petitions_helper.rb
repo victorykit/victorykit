8afcdea582f7961e5192da35efa7a1f518c1e2f9
Martin Andersen <manderdev@gmail.com>
2012-10-01 22:03:11 -0600

    added admin/dashboard with some email info from heartbeat page and an nps chart with views by month/week/day/hour

--- app/helpers/admin/petitions_helper.rb
+++ app/helpers/admin/petitions_helper.rb
@@ -1,7 +1,14 @@
 module Admin::PetitionsHelper
   
-  def link_to_self_with_param option, option_items, delimiter
-    option_items.collect { |k,v| link_to k, url_for(params.merge(option => v))}.join(delimiter).html_safe
+  def link_to_self_with_param param_name, options, delimiter
+    options = Hash[options.zip(options)] if options.is_a? Array
+    options.collect do |label, value|
+      if params[param_name] == value
+        label
+      else
+        link_to label, url_for(params.merge(param_name => value))
+      end
+    end.join(delimiter).html_safe
   end
   
 end
