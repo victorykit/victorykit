6083d244d73dd306251f89d55401985099328e9e
Martin Andersen <manderdev@gmail.com>
2012-10-04 11:01:50 -0600

    added list of top/bottom three petitions by nps to admin/dashboard; consolidated some nps calcs into models/metrics/nps.rb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :require_admin
   newrelic_ignore
 
-  helper_method :heartbeat, :nps_chart_url, :nps_chart_from_options
+  helper_method :heartbeat, :nps_chart_url, :timeframe_options, :petition_extremes
 
   def heartbeat
     heartbeat = Admin::Heartbeat.new
@@ -16,33 +16,45 @@ class Admin::DashboardController < ApplicationController
     }
   end
 
-  def nps_chart_from_options
+  def timeframe_options
     ["month", "week", "day", "hour"]
   end
 
   def nps_chart_url
-    from_param = validate_param nps_chart_from_options, params[:f], 'week', "Filter not recognized: #{params[:f]}"
-    from = "1#{from_param}"
-<<-url
+    from = "1#{timeframe}"
+u = <<-url
 http://graphite.watchdog.net/render?\
-target=movingAverage(stats.gauges.victorykit.nps,1440)&\
-target=movingAverage(stats.gauges.victorykit.nps,60)&\
+target=alias(movingAverage(stats.gauges.victorykit.nps,1440),"moving average (daily)")&\
+target=alias(movingAverage(stats.gauges.victorykit.nps,60), "moving average (hourly)")&\
 from=-#{from}&\
 fontName=Helvetica&fontSize=12&title=New%20members%20per%20email%20sent&\
 bgcolor=white&fgcolor=black&colorList=darkgray,red&\
 lineWidth=3&\
-height=400&width=800&
+height=400&width=800&\
 format=svg
 url
+u.strip
   end
 
-  def validate_param options, value, default, error
+  def timeframe
+    from_param = scrub_param timeframe_options, params[:f], 'week', "Filter not recognized: #{params[:f]}"
+  end
+
+  def scrub_param options, value, default, error
     if value and not options.include? value
       flash.now[:error] = error unless options.include? value
       default
     else
       value || default
     end
+  end
+
+  def petition_extremes
+    limit = 3
+    nps = Metrics::Nps.new.timespan 1.send(timeframe).ago..Time.now
+    sorted = nps.sort_by { |n| n[:nps] }.reverse
+    extremes = sorted.first(limit) + sorted.last(limit)
+    Petition.select("id, title").where("id in (?)", extremes.map{ |n| n[:petition_id] }).zip(extremes)
   end
 
 end
