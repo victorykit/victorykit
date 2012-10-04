dc22f6f5bbb085e0654165a8a61afd31846eeeb4
Brian Guthrie <btguthrie@gmail.com>
2012-10-04 18:18:02 -0400

    Migration to update old codes.

---
+++ db/migrate/20121004214008_generate_unique_referral_codes.rb
@@ -0,0 +1,17 @@
+class GenerateUniqueReferralCodes < ActiveRecord::Migration
+  def up
+    old_code_regex = /^(\d+)\.(.*?)$/
+
+    ReferralCode.find_in_batches do |batch|
+      batch.each do |c|
+        if c.code =~ old_code_regex
+          c.code = SecureRandom.urlsafe_base64(8)
+          c.save(validate: false)
+        end
+      end
+    end
+  end
+
+  def down
+  end
+end
