3af39ea2047e77ef78caafbd17bd1a880275a725
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-10-10 14:30:12 -0400

    bye bye smoke, we`ll miss you

--- spec/smoke/privacy_policy.rb
+++
@@ -1,8 +0,0 @@
-require 'smoke_spec_helper.rb'
-
-describe 'Privacy policy page' do
-	it 'should have a title' do
-		go_to HOST_URL + "/privacy"
-		element(:class => "privacy_title").text.downcase.should == "privacy policy"
-	end
-end
