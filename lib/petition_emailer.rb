edc603941b0fb62b0932e6af60cc9a8316a2e923
Aaron Swartz <me@aaronsw.com>
2012-10-15 10:46:44 -0400

    optimize email sender

--- lib/petition_emailer.rb
+++ lib/petition_emailer.rb
@@ -2,15 +2,16 @@
 class PetitionEmailer
   extend Whiplash
   
-  def self.send
-    member = Member.random_and_not_recently_contacted
-    self.send_to member if member
+  def self.send(n)
+    Member.random_and_not_recently_contacted(n).each  do |member|
+      self.send_to member if member
+    end
   end
 
   def self.send_to member
     interesting = Petition.find_interesting_petitions_for(member)
     return unless interesting.any?
-
+    
     id = self.spin_for interesting, member
     petition = Petition.find_by_id(id)
     ScheduledEmail.new_petition(petition, member)
--- worker/email_scheduler.rb
+++ worker/email_scheduler.rb
@@ -2,24 +2,24 @@ require 'scheduled_email'
 require 'petition_emailer'
 
 WEEK = 60*60*24*7
+BATCH_SIZE = 100
 
 class EmailScheduler
-  
   def self.schedule_email
+    max_emails_per_week = Member.count.to_f
     MailerProcessTracker.in_transaction do
       while 1
-        max_emails_per_week = Member.count.to_f
         last_email = Time.now
-        send_email
-        interval = WEEK/max_emails_per_week - (Time.now-last_email)
+        send_email(BATCH_SIZE)
+        interval = WEEK/(max_emails_per_week/BATCH_SIZE) - (Time.now-last_email)
         puts "Sleeping " + interval.to_s
         sleep(interval) unless interval < 0
       end
     end
   end
 
-  def self.send_email
-    PetitionEmailer.send
+  def self.send_email(n)
+    PetitionEmailer.send(n)
     MailerProcessTracker.update
   end
 end
