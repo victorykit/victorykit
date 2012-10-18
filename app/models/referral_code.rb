236d788281853498328b12155620b8ccdb503c6c
Martin Andersen <manderdev@gmail.com>
2012-10-18 14:38:54 -0600

    re-applied test name change from 'facebook default image' to 'facebook default image 2' that de2b935e seems to have misplaced

--- app/models/referral_code.rb
+++ app/models/referral_code.rb
@@ -71,7 +71,7 @@ class ReferralCode < ActiveRecord::Base
 
   def test_names
     { :title => "petition #{petition.id} #{title_type} title",
-      :image => petition.petition_images.any? ? "petition #{petition.id} #{title_type} image" : "default facebook image" }
+      :image => petition.petition_images.any? ? "petition #{petition.id} #{title_type} image" : "default facebook image 2" }
   end
 
 end
