29f2e03b2dc2126cf9d1b3d43d598eced7b9c406
Martin Andersen <manderdev@gmail.com>
2012-10-19 10:59:57 -0600

    scheduled_email#new_petition and #send_preview now share code path to compose email

--- app/mailers/scheduled_email.rb
+++ app/mailers/scheduled_email.rb
@@ -8,26 +8,8 @@ class ScheduledEmail < ActionMailer::Base
   def new_petition(petition, member)
     SentEmail.transaction do
       begin
-        sent_email = log_sent_email(member, petition)
-        sent_email_hash = sent_email.to_hash
-
-        @petition = petition
-        @member = member
-        @petition_link = petition_url(petition, n: sent_email_hash)
-        @unsubscribe_link = new_unsubscribe_url(Unsubscribe.new, n: sent_email_hash)
-        @tracking_url = new_pixel_tracking_url(n: sent_email_hash)
-        @image_url = email_experiment.image_url
-        @hide_demand_progress_introduction = email_experiment.hide_demand_progress_intro?
-        @demand_progress_introduction_location = email_experiment.demand_progress_introduction_location
-        @ask_to_sign_text = email_experiment.ask_to_sign_text
-        @box_location = email_experiment.box_location
-        @show_ps_with_plain_text = email_experiment.show_ps_with_plain_text
-        @show_less_prominent_unsubscribe_link = email_experiment.show_less_prominent_unsubscribe_link
-        @font_size_of_petition_link = "font-size:#{email_experiment.font_size_of_petition_link};"
-        @button_color = "background:#{email_experiment.button_color_for_petition_link};"
-        headers["List-Unsubscribe"] = "mailto:unsubscribe+" + sent_email_hash + "@appmail.watchdog.net"
-
-        mail(subject: email_experiment.subject, from: Settings.email.from_address, to: "\"#{member.full_name}\" <#{member.email}>").deliver
+        email_record = SentEmail.create!(email: member.email, member: member, petition: petition)
+        compose(petition, member, email_record, petition_link(petition, email_record)).deliver
       rescue => exception
         Airbrake.notify(exception)
         Rails.logger.error "exception sending email: #{exception} #{exception.backtrace.join}"
@@ -37,24 +19,56 @@ class ScheduledEmail < ActionMailer::Base
   end
 
   def send_preview(petition, member)
-    @petition = petition
-    @member = member
-    @petition_link = petition.persisted? ? petition_url(petition) : "PETITION LINK GOES HERE"
-    @unsubscribe_link = new_unsubscribe_url(Unsubscribe.new)
-    @tracking_url = new_pixel_tracking_url
-    @image_url = petition.petition_images.any? ? petition.petition_images.first.url : nil
-    @ask_to_sign_text = "Please, click here to sign now!"
-    @box_location = 'top' #hardcoded for now. spining here might not be relevant
-    mail(subject: petition.title, from: Settings.email.from_address, to: "\"#{member.full_name}\" <#{member.email}>", :template_name => 'new_petition').deliver
+    SentEmail.transaction do
+      begin
+        email_record = SentEmail.new(email: member.email, member: member, petition: petition)
+        class <<email_record
+          def to_hash
+            "preview"
+          end
+        end
+        compose(petition, member, email_record, petition_link(petition, email_record, true)).deliver
+        # always rollback for preview
+        raise ActiveRecord::Rollback
+      end
+    end
   end
 
   private
-  def email_experiment
-    @email_experiment ||= EmailExperiments.new(@sent_email)
+
+  def petition_link petition, email, is_preview=false
+    if is_preview and not petition.persisted?
+      return "PETITION LINK GOES HERE"
+    end
+    return petition_url petition, n: email.to_hash
   end
 
-  def log_sent_email(member, petition)
-    @sent_email = SentEmail.create!(email: member.email, member: member, petition: petition)
+  def compose petition, member, email, petition_link
+    email_hash = email.to_hash
+    email_experiment = EmailExperiments.new(email)
+
+    @petition = petition
+    @member = member
+    @petition_link = petition_link
+
+    @unsubscribe_link = new_unsubscribe_url(Unsubscribe.new, n: email_hash)
+    @tracking_url = new_pixel_tracking_url(n: email_hash)
+    @image_url = email_experiment.image_url
+    @hide_demand_progress_introduction = email_experiment.hide_demand_progress_intro?
+    @demand_progress_introduction_location = email_experiment.demand_progress_introduction_location
+    @ask_to_sign_text = email_experiment.ask_to_sign_text
+    @box_location = email_experiment.box_location
+    @show_ps_with_plain_text = email_experiment.show_ps_with_plain_text
+    @show_less_prominent_unsubscribe_link = email_experiment.show_less_prominent_unsubscribe_link
+    @font_size_of_petition_link = "font-size:#{email_experiment.font_size_of_petition_link};"
+    @button_color = "background:#{email_experiment.button_color_for_petition_link};"
+    headers["List-Unsubscribe"] = "mailto:unsubscribe+" + email_hash + "@appmail.watchdog.net"
+
+    mail = mail(
+      subject: email_experiment.subject,
+      from: Settings.email.from_address,
+      to: "\"#{member.full_name}\" <#{member.email}>",
+      template_name: 'new_petition')
   end
 
 end
