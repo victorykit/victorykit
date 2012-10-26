b573765be32ae01ebc8043ac1b8af2179c9fc4c7
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-10-26 16:32:20 -0400

    updates sent emails while merging members

--- db/migrate/20121025143023_merge_members.rb
+++ db/migrate/20121025143023_merge_members.rb
@@ -15,26 +15,10 @@ class MergeMembers < ActiveRecord::Migration
       if ids.size > 1
         first, rest = ids[0], ids[1..-1]
         Signature.where(member_id: rest).update_all(member_id: first)
+        SentEmail.where(member_id: rest).update_all(member_id: first)
         Member.where(id: rest).delete_all
       end
     end
-
-    # seen.select {|email, count| count > 1}.each do |email, count|
-    # end
-
-    #     next if trash.include? m
-    #     duplicates =  Member.order('id asc').
-    #       find(:all, :conditions => ['lower(email)=?', m.email.downcase])
-    #     first = duplicates.first
-    #     rest = duplicates - [first]
-    #     (rest.map(&:signatures).reduce(&:+) || []).each do |s|
-    #       s.update_attributes(:member => first)
-    #     end
-    #     trash.concat rest
-    #     rest.map{ |x|x.signatures=[]; }
-    #     rest.map(&:destroy)
-    #   end
-    # end
   end
 
   def down
