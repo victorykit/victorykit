97215ffb41e00fcee6035a709e47248c3dbbc638
Aaron Swartz <me@aaronsw.com>
2012-11-04 21:57:38 -0500

    FASTER again!

--- app/models/member.rb
+++ app/models/member.rb
@@ -18,7 +18,7 @@ class Member < ActiveRecord::Base
       WHERE members.id NOT IN (
         SELECT member_id 
         FROM sent_emails 
-        WHERE created_at > now() - interval '1 week'
+        WHERE created_at > now() - interval '48 hours'
       ) AND members.id NOT IN (
         SELECT member_id
         FROM signatures
