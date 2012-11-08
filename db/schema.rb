8502030e59a7c9f9c64ef533d32b361c99075e62
Michelle D'Souza <michd2005@gmail.com>
2012-11-08 15:35:44 -0700

    Migrating short_summary data from petitions to petition_summaries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107225527) do
+ActiveRecord::Schema.define(:version => 20121108222518) do
 
   create_table "bounced_emails", :force => true do |t|
     t.text     "raw_content"
