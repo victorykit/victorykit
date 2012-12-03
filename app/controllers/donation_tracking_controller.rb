f24c08a2e559cfa325445cebd7591863348af67e
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-03 04:59:30 -0500

    paypal ipn implementation

--- app/controllers/donation_tracking_controller.rb
+++ app/controllers/donation_tracking_controller.rb
@@ -14,23 +14,16 @@ class DonationTrackingController < ApplicationController
     render :text => ''
   end
 
-  def show
-    # should we move token and uri to env variables?
-    token = "NJxH2Yzm_ONgyOOzgyHBSwBrq72X_KltZF1_QutkfBvQMmaJr4OuVz9uz-G"
-    uri = URI('https://www.sandbox.paypal.com/cgi-bin/webscr')
-
+  def paypal
+    uri = URI(Settings.paypal.uri)
     Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       req = Net::HTTP::Post.new(uri.request_uri)
+      req.set_form_data(params.merge({:cmd => '_notify-validate'}))
 
-      req.set_form_data(
-        'cmd' => '_notify-synch', 
-        'tx' => params[:tx], 
-        'at' => token)
       req['host'] = uri.host
       req['content-type'] = 'application/x-www-form-urlencoded'
 
-      res = http.request(req) # Net::HTTPResponse object
+      res = http.request(req) 
       Rails.logger.info(res.body)
     end
 
