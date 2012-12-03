e0a8adfa362768efd3e2c2a52f4f6856c53f423e
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-03 16:25:16 -0500

    adds validation to donation click

--- app/models/donation_click.rb
+++ app/models/donation_click.rb
@@ -2,4 +2,5 @@ class DonationClick < ActiveRecord::Base
   belongs_to :petition
   belongs_to :member
   attr_accessible :petition, :member, :referral_code_id, :amount
+  validates_presence_of :petition, :member, :referral_code_id
 end
