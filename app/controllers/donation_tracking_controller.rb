6cc0d17f53dff56c89725a0a22275d2221320e19
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-04 15:14:31 -0500

    inline status

--- app/controllers/donation_tracking_controller.rb
+++ app/controllers/donation_tracking_controller.rb
@@ -4,12 +4,11 @@ class DonationTrackingController < ApplicationController
 
   def create
     win! :donation
-    status = DonationClick.create({
-      petition: Petition.find_by_id(params[:petition_id]),
+    render(:nothing => true, :status => DonationClick.create({
+      referral_code: ReferralCode.find_by_code(params[:referral_code]),
       member: Signature.find_by_id(params[:signature_id]).try(:member),
-      referral_code: ReferralCode.find_by_code(params[:referral_code])
-    }).valid? ? 200 : 500
-    render(:nothing => true, :status => status)
+      petition: Petition.find_by_id(params[:petition_id])
+    }).valid? ? 200 : 500)
   end
 
   def paypal
