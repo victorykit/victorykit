6ee5577ce0e12c66be4da6e6190453b477fb2f92
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-04 13:02:11 -0500

    create improved

--- app/controllers/donation_tracking_controller.rb
+++ app/controllers/donation_tracking_controller.rb
@@ -4,14 +4,12 @@ class DonationTrackingController < ApplicationController
 
   def create
     win! :donation
-    @petition = Petition.find params[:petition_id]
-    @member = Signature.find(params[:signature_id]).member if params[:signature_id].present?
-    referral_code = ReferralCode.find_by_code(params[:referral_code]) if params[:referral_code].present?
-    @referral_code_id = referral_code.id if referral_code.present?
-
-    DonationClick.create(referral_code_id: @referral_code_id, petition: @petition, member: @member)
-
-    render :text => ''
+    status = DonationClick.create({
+      petition: Petition.find_by_id(params[:petition_id]),
+      member: Signature.find_by_id(params[:signature_id]).try(:member),
+      referral_code: ReferralCode.find_by_code(params[:referral_code])
+    }).valid? ? 200 : 500
+    render(:nothing => true, :status => status)
   end
 
   def paypal
@@ -19,14 +17,11 @@ class DonationTrackingController < ApplicationController
     Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
       req = Net::HTTP::Post.new(uri.request_uri)
       req.set_form_data(params.merge({:cmd => '_notify-validate'}))
-
       req['host'] = uri.host
       req['content-type'] = 'application/x-www-form-urlencoded'
-
       res = http.request(req) 
       Rails.logger.info(res.body)
     end
-
     render :text => 'OK'
   end
 end
