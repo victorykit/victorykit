5fdb7effddb16d14dfd10479899e3e683eb80b51
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-04 15:29:11 -0500

    drying up spec

--- spec/controllers/donation_tracking_controller_spec.rb
+++ spec/controllers/donation_tracking_controller_spec.rb
@@ -1,50 +1,29 @@
 describe DonationTrackingController do
 
   describe '#create' do
-    let(:petition) { create(:petition) }
-    let(:signature) { create(:signature, :member => create(:member)) }
-    let(:referral_code) { create(:referral_code) }
+    let(:petition_id) { create(:petition).id }
+    let(:signature_id) { create(:signature, :member => create(:member)).id }
+    let(:referral_code) { create(:referral_code).code }
+    
+    let(:params) {{
+      :petition_id => petition_id,
+      :signature_id => signature_id,
+      :referral_code => referral_code
+    }}
+
+    before { post(:create, params) }
 
     context 'all required params provided' do
-      before do
-        post(:create, {
-          :petition_id => petition.id,
-          :signature_id => signature.id,
-          :referral_code => referral_code.code
-        })
-      end
       it { should respond_with 200 }
     end
 
-    context 'no petition_id' do
-      before do
-        post(:create, {
-          :signature_id => signature.id,
-          :referral_code => referral_code
-        })
+    [:petition_id, :signature_id, :referral_code].each do |param|
+      context "missing #{param}" do
+        let(param) { nil }
+        it { should respond_with 500 }
       end
-      it { should respond_with 500 }
-    end
-    
-    context 'no signature_id' do
-      before do
-        post(:create, {
-          :petition_id => petition.id,
-          :referral_code => referral_code
-        })
-      end
-      it { should respond_with 500 }
     end
 
-    context 'no referral_code' do
-      before do
-        post(:create, {
-          :signature_id => signature.id,
-          :petition_id => petition.id
-        })
-      end
-      it { should respond_with 500 }
-    end
   end
 
 end
