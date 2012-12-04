64faaca0a4176a20cf9ef02e3c8144753d837f82
Jim Arnold <jim@thoughtworks.com>
2012-12-04 17:13:17 -0500

    added access token for requests from cloudmailin

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -5,16 +5,24 @@ class IncomingMailsController < ApplicationController
     skip_before_filter :verify_authenticity_token
 
     def create
-      message = Mail.new(params[:message])
-      to_address = params[:to].to_s.gsub(/[<>]/, '')
-
-      if (to_address and to_address.to_s.start_with? 'unsubscribe')
-        Rails.logger.info "Received unsubscribe email"
-        EmailProcessor.handle_exceptional_email(message.to_s, to_address.to_s, 'unsubscribe')
-        render :text => 'success', :status => 200
+      if not authorized
+        render_403
       else
-        Rails.logger.info "Message failed #{message} from incorrect to address: #{to_address}"
-        render :text => "Message failed #{message} from incorrect to address: #{to_address}", :status => 404
+        message = Mail.new(params[:message])
+        to_address = params[:to].to_s.gsub(/[<>]/, '')
+
+        if (to_address and to_address.to_s.start_with? 'unsubscribe')
+          Rails.logger.info "Received unsubscribe email"
+          EmailProcessor.handle_exceptional_email(message.to_s, to_address.to_s, 'unsubscribe')
+          render :text => 'success', :status => 200
+        else
+          Rails.logger.info "Message failed #{message} from incorrect to address: #{to_address}"
+          render :text => "Message failed #{message} from incorrect to address: #{to_address}", :status => 404
+        end
       end
+    end
+
+    def authorized
+       Settings.cloudmailin.token.nil? ? true : (params['token'] == Settings.cloudmailin.token)
     end
 end
--- spec/controllers/incoming_mails_controller_spec.rb
+++ spec/controllers/incoming_mails_controller_spec.rb
@@ -4,16 +4,24 @@ describe IncomingMailsController do
 
   describe "create" do
     it "should call MailReceiver.receive_unsubscribe_email if to field starts with 'unsubscribe'" do
-      Mail::Message.any_instance.stub(:to_s => 'email_to_s')
+      Mail::Message.any_instance.stub(to_s: 'email_to_s')
       EmailProcessor.should_receive(:handle_exceptional_email).with('email_to_s', 'unsubscribe+hash@appmail.watchdog.net', 'unsubscribe')
 
-      get :create, {:to => "unsubscribe+hash@appmail.watchdog.net", :message => "email"}
+      get :create, {to: "unsubscribe+hash@appmail.watchdog.net", message: "email"}
     end
 
 
     it "should return 404 if to field doesn`t start from none of above" do
-      get :create, {:to => "somethingelse+hash@appmail.watchdog.net", :message => "email"}
+      get :create, {to: "somethingelse+hash@appmail.watchdog.net", message: "email"}
       response.status.should == 404
+    end
+
+    it "should only allow requests with a valid token" do
+      Settings.cloudmailin.token = "foo"
+      get :create, {to: "unsubscribe+hash@appmail.watchdog.net", message: "email"}
+      response.status.should == 403
+      get :create, {to: "unsubscribe+hash@appmail.watchdog.net", message: "email", token: "foo"}
+      response.status.should == 200
     end
 
   end
