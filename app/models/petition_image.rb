3a527af51503e02c4b3b6800f4ea89263f1803c1
Rafael Petry <rafael@rafaelpetry.com>
2012-12-04 16:51:59 -0500

    Uploading petition images do S3 bucket.

--- app/models/petition_image.rb
+++ app/models/petition_image.rb
@@ -2,4 +2,10 @@ class PetitionImage < ActiveRecord::Base
   attr_accessible :url
   attr_accessible :url, :as => :admin
   belongs_to :petition
+
+  def s3_object_name
+    hash = Digest::MD5.hexdigest("#{id} #{url}")
+    extension = url.split('.').last
+    [hash, extension].join('.')
+  end
 end
