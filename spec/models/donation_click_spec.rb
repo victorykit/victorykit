5d6344bc91d06e086ece15c5584eb67a0054e069
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-05 16:14:19 -0500

    confirm payment

--- spec/models/donation_click_spec.rb
+++ spec/models/donation_click_spec.rb
@@ -9,4 +9,20 @@ describe DonationClick do
   it { should validate_presence_of :petition }
   it { should validate_presence_of :member }
   it { should validate_presence_of :referral_code }
+
+  describe '.confirm_payment' do
+    let(:donation) { mock }
+    let(:donator) { stub }
+
+    before do
+      Member.stub(:where).with(:email => 'bob@gmail.com').and_return([donator])
+      DonationClick.stub(:where).with(:member_id => donator, :amount => nil).
+        and_return([donation])
+    end
+    
+    it 'updates amount' do
+      donation.should_receive(:update_attributes).with(:amount => 30.0)
+      DonationClick.confirm_payment(30.0, 'bob@gmail.com')
+    end
+  end
 end
