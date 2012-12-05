1acbc97b4433847bd75344057aabc218ba3a1833
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-05 10:28:57 -0500

    persisting amount

--- app/controllers/donation_tracking_controller.rb
+++ app/controllers/donation_tracking_controller.rb
@@ -12,15 +12,23 @@ class DonationTrackingController < ApplicationController
   end
 
   def paypal
+    # extract paypal stuff to a class
     uri = URI(Settings.paypal.uri)
     Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
       req = Net::HTTP::Post.new(uri.request_uri)
       req.set_form_data(params.merge({:cmd => '_notify-validate'}))
       req['host'] = uri.host
       req['content-type'] = 'application/x-www-form-urlencoded'
-      res = http.request(req) 
+      res = http.request(req)
       Rails.logger.info(res.body)
+      if res.body == 'VERIFIED'
+        donator = Member.where(:email => params[:payer_email]).first
+        DonationClick.where(:member_id => donator, :amount => nil).last.
+          update_attributes(:amount => params[:payment_gross])
+        render(:nothing => true, :status => 200)
+      else
+        render(:nothing => true, :status => 500)
+      end
     end
-    render(:text => 'OK')
   end
 end
