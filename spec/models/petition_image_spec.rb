271b901f24eb826e2849f91e6cd38aa871dc48e8
Rafael Petry <rafael@rafaelpetry.com>
2012-12-05 17:05:06 -0500

    Removed petition image id from string used to create the S3 object key hash.

--- spec/models/petition_image_spec.rb
+++ spec/models/petition_image_spec.rb
@@ -1,8 +1,8 @@
 describe PetitionImage do
   describe "s3 object key" do
-    it "is a hash of id and url with a file extension" do
-      image = create(:petition_image, :url => 'foo.jpg')
-      image.s3_object_key.should == Digest::MD5.hexdigest("#{image.id} foo.jpg") + ".jpg"
+    it "is a hash of the url with a file extension" do
+      image = build(:petition_image, :url => 'foo.jpg')
+      image.s3_object_key.should == Digest::MD5.hexdigest("foo.jpg") + ".jpg"
     end
   end
 
