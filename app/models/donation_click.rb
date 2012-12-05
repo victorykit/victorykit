0de302cae49624d2bd9f5bee88f867c617fa67e0
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-05 16:14:19 -0500

    confirm payment

--- app/models/donation_click.rb
+++ app/models/donation_click.rb
@@ -4,4 +4,11 @@ class DonationClick < ActiveRecord::Base
   belongs_to :referral_code
   attr_accessible :member, :petition, :referral_code, :amount
   validates_presence_of :petition, :member, :referral_code
+
+  def self.confirm_payment(amount, email)
+    donator = Member.where(:email => email).first
+    donation = DonationClick.where(:member_id => donator, :amount => nil).last
+    donation.update_attributes(:amount => amount)
+  end
+
 end
