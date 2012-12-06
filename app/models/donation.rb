6ebf6d9fe2bfdedc9a9c6e9eebbec672a48ae80d
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-06 17:24:47 -0500

    logs payment confirmation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,8 +6,11 @@ class Donation < ActiveRecord::Base
   validates_presence_of :petition, :member, :referral
 
   def self.confirm_payment(amount, email)
+    Rails.logger.info(">>> CONFIRM PAYMENT [#{amount}, #{email}]")
     donator = Member.where(:email => email).first
+    Rails.logger.info(">>> DONATOR: #{ donator }")
     donation = Donation.where(:member_id => donator, :amount => nil).last
+    Rails.logger.info(">>> DONATION: #{ donation }")
     donation.update_attributes(:amount => amount)
   end
 
