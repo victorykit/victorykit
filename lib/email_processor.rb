1e2c665e151372945f995e9505ad5fdb19657222
Jim Arnold <jim@thoughtworks.com>
2012-12-06 15:51:45 -0500

    unsubscribe members even if original SentEmail cannot be found (some hashes are coming through lowercased)

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -2,26 +2,27 @@ require 'sent_email_hasher'
 
 class EmailProcessor < ActionMailer::Base
 
-  def handle_exceptional_email(email, to_address, cause)
+  def handle_exceptional_email(email, from_address, to_address, cause)
     begin
       if (to_address)
-        address, domain = to_address.to_s.split("@")
+        address, domain = to_address.split("@")
         hash = address.gsub(/^[\w\.\-]+\+?/, "") #should remove everything up to and including the first '+'.  Hashes can contain pluses! abc+23+asd@foo.com
         if sent_email = SentEmail.find_by_hash(hash)
           bounced_email = BouncedEmail.new(raw_content: email.to_s, sent_email: sent_email)
           bounced_email.save!
-
-          unsubscribe = Unsubscribe.new(email: sent_email.email, cause: cause)
-          unsubscribe.member = Member.find_by_email(sent_email.email) 
-          if unsubscribe.member
-            unsubscribe.save!
-          end
+        else
+          Rails.logger.warn "Failed to find a SentEmail for hash #{hash}. Possibly lowercased?"
+        end
+        unsubscribe = Unsubscribe.new(email: from_address, cause: cause)
+        unsubscribe.member = Member.find_by_email(from_address)
+        if unsubscribe.member
+          unsubscribe.save!
         end
       end
     rescue => error
       Airbrake.notify(error)
       puts "Error in processing exceptional mail #{error}"
-    end  
+    end
   end
 
 end
--- spec/lib/email_processor_spec.rb
+++ spec/lib/email_processor_spec.rb
@@ -3,7 +3,7 @@ describe EmailProcessor do
   let!(:member) {create :member, email: sent_email.email}
 
   it "should create a record in bounced_emails table" do
-    EmailProcessor.handle_exceptional_email("email_content", "bounce+#{sent_email.to_hash}@appmail.watchdog.net", "bounced")
+    EmailProcessor.handle_exceptional_email("email_content", sent_email.email, "bounce+#{sent_email.to_hash}@appmail.watchdog.net", "bounced")
     bounced_mail_records = BouncedEmail.find(:all)
     bounced_mail_records.size.should == 1
     bounced_mail_records[0].raw_content.should == "email_content"
@@ -11,7 +11,16 @@ describe EmailProcessor do
   end
 
   it "should create a record in unsubscribes table if we have a member with such email" do
-    EmailProcessor.handle_exceptional_email("email_content", "unsubscribe+#{sent_email.to_hash}@appmail.watchdog.net", "unsubscribe")
+    EmailProcessor.handle_exceptional_email("email_content", sent_email.email, "unsubscribe+#{sent_email.to_hash}@appmail.watchdog.net", "unsubscribe")
+    unsubscribe_records = Unsubscribe.find(:all)
+    unsubscribe_records.size.should == 1
+    unsubscribe_records[0].email.should == sent_email.email
+    unsubscribe_records[0].cause.should == 'unsubscribe'
+    unsubscribe_records[0].member_id.should == member.id
+  end
+
+  it 'should still unsubscribe member even if sent email could not be found' do
+    EmailProcessor.handle_exceptional_email("email_content", sent_email.email, "unsubscribe+123_foo@appmail.watchdog.net", "unsubscribe")
     unsubscribe_records = Unsubscribe.find(:all)
     unsubscribe_records.size.should == 1
     unsubscribe_records[0].email.should == sent_email.email
