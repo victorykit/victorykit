26229cfa1398b5473da4590afdbff866c4bd5eef
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2012-12-06 17:56:52 -0500

    oh, I forgot to find member by hash code

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -5,9 +5,9 @@ class Donation < ActiveRecord::Base
   attr_accessible :member, :petition, :referral, :amount
   validates_presence_of :petition, :member, :referral
 
-  def self.confirm_payment(amount, email)
-    Rails.logger.info(">>> CONFIRM PAYMENT [#{amount}, #{email}]")
-    donator = Member.where(:email => email).first
+  def self.confirm_payment(amount, hash)
+    Rails.logger.info(">>> CONFIRM PAYMENT [#{amount}, #{hash}]")
+    donator = Member.find_by_hash(hash).first
     Rails.logger.info(">>> DONATOR: #{ donator }")
     donation = Donation.where(:member_id => donator, :amount => nil).last
     Rails.logger.info(">>> DONATION: #{ donation }")
