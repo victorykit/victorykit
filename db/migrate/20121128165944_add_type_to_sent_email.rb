edc8c2c70c18ca289e28ffe2bf37928721972d35
Jim Arnold <jim@thoughtworks.com>
2012-12-07 15:27:00 -0500

    Revert "added type column to SentEmail in preparation for subclasses"
    This reverts commit 48bb8240362051be7253784e06fed4026faa01c7.

--- db/migrate/20121128165944_add_type_to_sent_email.rb
+++
@@ -1,6 +0,0 @@
-class AddTypeToSentEmail < ActiveRecord::Migration
-  def change
-    add_column :sent_emails, :type, :string
-    execute "UPDATE sent_emails SET type = 'ScheduledSentEmail'"
-  end
-end
