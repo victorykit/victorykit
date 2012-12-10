d856afecaa46fe7873be289b01110fe9238088a9
Jim Arnold <jim@thoughtworks.com>
2012-12-10 12:44:03 -0500

    pulled ScheduledEmail members back up to SentEmail until we can migrate the type data

--- app/models/sent_email.rb
+++ app/models/sent_email.rb
@@ -1,9 +1,10 @@
 class SentEmail < ActiveRecord::Base
-  attr_accessible :email, :member, :petition
+  attr_accessible :email, :member, :petition, :opened_at, :clicked_at, :signature
   belongs_to :petition
   belongs_to :member
   has_many :email_experiments
   has_one :unsubscribe
+  belongs_to :signature
 
   def to_hash
     SentEmailHasher.generate self.id
@@ -13,5 +14,14 @@ class SentEmail < ActiveRecord::Base
 
   def self.find_by_hash(hash)
     self.by_hash(hash).first
+  end
+
+  def already_clicked?
+    !self.clicked_at.nil?
+  end
+
+  def track_visit!
+    self.update_attributes(clicked_at: Time.now) unless already_clicked?
+    $statsd.increment "emails_clicked.count"
   end
 end
