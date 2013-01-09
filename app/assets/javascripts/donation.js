1908e9fcf8711a8dbc5351c2ba0a01a3e03d9e36
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-01-09 16:14:15 -0500

    changing the way we redirect to paypal

--- app/assets/javascripts/donation.js
+++ app/assets/javascripts/donation.js
@@ -4,25 +4,30 @@ var donation = (function() {
       petition_id: VK.petition_id, 
       referral_code: VK.ref_code,
       signature_id: VK.signature_id
+    }, function() {
+      window.location = buildPaypalUrl();
     });
+  }
+
+  function buildPaypalUrl() {
+    var params = [
+      'cmd=_donations',
+      'business=info@demandprogress.org',
+      'lc=US',
+      'item_name=Support%20Watchdog.net',
+      'item_number='+$.cookie('member_id'),
+      'no_note=1',
+      'no_shipping=1',
+      'currency_code=USD',
+      'bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted',
+      'notify_url=http%3A%2F%2F'+location.host+'%2Fpaypal'
+    ].join('&');
+    return VK.paypal_uri+'?'+params;
   }
 
   return {
     init: function() {
-      var params = [
-        'cmd=_donations',
-        'business=info@demandprogress.org',
-        'lc=US',
-        'item_name=Support%20Watchdog.net',
-        'item_number='+$.cookie('member_id'),
-        'no_note=1',
-        'no_shipping=1',
-        'currency_code=USD',
-        'bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted',
-        'notify_url=http%3A%2F%2F'+location.host+'%2Fpaypal'
-      ].join('&');
-      var url = VK.paypal_uri+'?'+params;
-      $('.donate_btn').attr('href', url).click(sendDonationTrackRequest);
+      $('.donate_btn').click(sendDonationTrackRequest);
     }
   };
 })();
