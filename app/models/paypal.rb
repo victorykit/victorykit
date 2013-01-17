f5b296c6a74eda388b58bb82b47bedfc9af8db12
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-01-17 15:29:16 -0500

    stubs network stuff for paypal spec

--- app/models/paypal.rb
+++ app/models/paypal.rb
@@ -1,16 +1,24 @@
 class Paypal
 
-  def self.verify_payment(params)
-    result = nil
-    uri = URI(Settings.paypal.uri)
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-      req = Net::HTTP::Post.new(uri.request_uri)
-      req.set_form_data(params.merge({:cmd => '_notify-validate'}))
-      req['host'] = uri.host
-      req['content-type'] = 'application/x-www-form-urlencoded'
-      result = http.request(req).body
+  class << self
+    def verify_payment(params)
+      send_post(params) == 'VERIFIED'
     end
-    result == 'VERIFIED'
+
+    private
+
+    def send_post(params)
+      result = nil
+      uri = URI(Settings.paypal.uri)
+      Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+        req = Net::HTTP::Post.new(uri.request_uri)
+        req.set_form_data(params.merge({:cmd => '_notify-validate'}))
+        req['host'] = uri.host
+        req['content-type'] = 'application/x-www-form-urlencoded'
+        result = http.request(req).body
+      end
+      result
+    end
   end
 
 end
