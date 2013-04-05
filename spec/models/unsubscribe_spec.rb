7a06fe08dd370f67cf7a0a785d342ba2ec47593e
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-04-05 10:54:47 -0400

    unsubs spec refactored

--- spec/models/unsubscribe_spec.rb
+++ spec/models/unsubscribe_spec.rb
@@ -1,19 +1,17 @@
 describe Unsubscribe do
-  describe "unsubscribe_member" do
-    subject { build(:unsubscribe) }
-    it { should validate_presence_of :email }
-    it { should be_a(Unsubscribe) }
-    it "should not be nil" do
-      Unsubscribe.unsubscribe_member(Member.new).should_not == nil
-    end
-    it "should have a cause of unsubscribed" do
-      Unsubscribe.unsubscribe_member(Member.new).cause.should == "unsubscribed"
-    end
+  it { should validate_presence_of :email }
+  it { should be_a Unsubscribe }
+
+  describe '.unsubscribe_member' do
+    subject { Unsubscribe.unsubscribe_member(Member.new) }
+    it { should_not be_nil }
+    its(:cause) { should == 'unsubscribed' }
   end
 
-  describe "analytics" do
-    it "should increment unsubscribe count on create" do
-      expect { create(:unsubscribe) }.to change{ $statsd.value_of("unsubscribes.count") }.from(0).to(1)
+  context 'analytics' do
+    specify do
+      expect { create(:unsubscribe) }.
+      to change{ $statsd.value_of('unsubscribes.count') }.from(0).to(1)
     end
   end
 end
