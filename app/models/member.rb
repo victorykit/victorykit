60b7e0f88629d2166e325b8888449f41b3e6f471
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-04-05 10:43:55 -0400

    member csv conversion

--- app/models/member.rb
+++ app/models/member.rb
@@ -67,6 +67,11 @@ class Member < ActiveRecord::Base
     signatures.where(petition_id: petition.try(:id)).first
   end
 
+  comma do
+    full_name
+    email
+  end
+
   private
   
   def self.active_subscription?(subscribe_date, unsubscribe_date)
