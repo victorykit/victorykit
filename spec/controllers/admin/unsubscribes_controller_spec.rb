c19f436e90b6cdc753ca966329e20a9a5625dc1b
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-04-06 14:13:04 -0400

    unsubscribes recent first

--- spec/controllers/admin/unsubscribes_controller_spec.rb
+++ spec/controllers/admin/unsubscribes_controller_spec.rb
@@ -1,7 +1,7 @@
 describe Admin::UnsubscribesController do
 
   describe '#index' do
-    before { Unsubscribe.stub(:paginate).and_return [:unsubs] }
+    before { Unsubscribe.stub_chain(:recent_first, :paginate).and_return [:unsubs] }
     before { get :index, {}, valid_admin_session }
     it { should respond_with 200 }
     it { assigns(:unsubscribes).should == [:unsubs] }
