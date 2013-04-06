db9d6854ea705b1724737f81445c409839135269
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-04-06 13:55:00 -0400

    unsubs page and admin restriction

--- spec/controllers/admin/unsubscribes_controller_spec.rb
+++ spec/controllers/admin/unsubscribes_controller_spec.rb
@@ -1,8 +1,10 @@
 describe Admin::UnsubscribesController do
+
   describe '#index' do
     before { Unsubscribe.stub(:paginate).and_return [:unsubs] }
-    before { get :index }
+    before { get :index, {}, valid_admin_session }
     it { should respond_with 200 }
     it { assigns(:unsubscribes).should == [:unsubs] }
   end
+
 end
