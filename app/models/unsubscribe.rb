5f64b223758efc36546031d0f9274a0f6fbcdc0b
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2013-04-07 17:05:12 -0400

    unsubscribe to csv

--- app/models/unsubscribe.rb
+++ app/models/unsubscribe.rb
@@ -4,10 +4,18 @@ class Unsubscribe < ActiveRecord::Base
   belongs_to :sent_email
   validates_presence_of :email
   
-  scope :recent_first, order('created_at DESC')
+  scope :between, ->(from, to) { where(:created_at => from..to) }
 
   def self.unsubscribe_member(member)
     Unsubscribe.create(email: member.email, cause: 'unsubscribed', member: member)
   end
 
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << %w{ Email Name }
+      all.each do |u|
+        csv << [u.member.email, u.member.full_name]
+      end
+    end
+  end
 end
