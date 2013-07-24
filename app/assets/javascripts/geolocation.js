4682cfa4b337c02aa1671587447799da202bfb0c
Nathan Woodhull <woodhull@gmail.com>
2013-07-24 15:00:24 -0400

    possible fix for semaphore failure.

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -32,23 +32,24 @@ var geolocation = (function() {
   function updateCountries() { update('#countries'); }
 
   function updateLocationVisibility() {
-      var val = $("#petition_location_type").val();
-      if      (val === 'us')     { showStates() }
-      else if (val === 'non-us') { showCountries() }
-      else                       { hideAll(); }
+    var val = $("#petition_location_type").val();
+    if      (val === 'us')     { showStates() }
+    else if (val === 'non-us') { showCountries() }
+    else                       { hideAll(); }
   }
 
   function update(selector) {
     var val = $(selector+' .chzn-select').val();
-    $('#location-details').val(val ? val.join(',') : '');
+    $('#petition_location_details').val(val ? val.join(',') : '');
   }
 
   function rebuildSelections() {
-    var loc = $('#location-options input[checked="checked"]').val();
+    var loc = $('#petition_location_type').val();
     var div = {'us': '#states', 'non-us': '#countries'}[loc];
     if(!div) { return; }
     var sel = ' .chzn-select';
-    var details = $('#location-details').val().split(',');
+    var details = $('#petition_location_details').val().split(',');
+    console.log("location details", details);
     $(details).each(function(i, e) {
       $(div+sel+' option[value="'+e+'"]').attr('selected', true);
       $(div+sel).trigger('liszt:updated');
