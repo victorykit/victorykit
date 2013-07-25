8812eff770eed7eb32aebad4827c14dfb337ed93
Brian Guthrie <btguthrie@gmail.com>
2013-07-25 19:39:51 -0400

    Attempting to clear up ambiguity in original comment.

--- app/models/signature_email.rb
+++ app/models/signature_email.rb
@@ -1,6 +1,8 @@
 class SignatureEmail < SentEmail
   attr_accessible :signature
-  #CONFUSION ALERT! This is not the signature that triggered the sending of this email. It is the signature that
-  #(may) arise from a referral off this email.
+  # CONFUSION ALERT!
+  # These emails are triggered by a user signing a petition, but the signature
+  # attribute is not that signature. It is the signature that may arise from a
+  # referral initiated from the sent email.
   belongs_to :signature
 end
