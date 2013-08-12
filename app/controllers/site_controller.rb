5a18da22f8067f4d2a04a883b6de8ecddd0063d4
Brian Guthrie <btguthrie@gmail.com>
2013-08-12 12:53:11 -0400

    Logic here was backwards.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,9 +2,9 @@ class SiteController < ApplicationController
 
   def index
     if current_user.present?
-      redirect_to login_path
-    else
       redirect_to admin_dashboard_path
+    else
+      redirect_to login_path
     end
   end
 
