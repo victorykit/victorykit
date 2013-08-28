a2f1af1c5ff17e039ba3a8e2bc61021c76a59353
Mark Steckel <mjs@change.org>
2013-08-28 12:22:41 -0700

    Remove crufty code post-Devise

--- app/validators/old_password_validator.rb
+++
@@ -1,9 +0,0 @@
-class OldPasswordValidator < ActiveModel::EachValidator
-  def validate_each(object, attribute, value)
-    old_user = User.find(object.id)
-#    unless old_user.authenticate value
-    unless sign_in old_user, attribute => value
-      object.errors[attribute] << (options[:message] || " is not your previous password")
-    end
-  end
-end
