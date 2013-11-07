23703fd579e54c7d3b852bf5e8d7c7bfadde10f3
Mark Steckel <mjs@fix.net>
2013-11-06 17:40:42 -0800

    Existing but unsubscribed members should be re-subscribed when they since a petition.
    Most of the logic was in place, however the member was not being saved...

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -27,38 +27,47 @@ class SignaturesController < ApplicationController
       member = Member.lookup(email).first || Member.new
       signature.created_member = member.new_record?
 
-      member.tap do |m|
-        m.first_name = signature.first_name
-        m.last_name = signature.last_name
-        m.email = email unless m.email
-        m.save
-      end
+member.tap do |m|
+  m.first_name = signature.first_name
+  m.last_name = signature.last_name
+  m.email = email unless m.email
+  m.save
+end
 
-      signature.member_id = member.id
+member.first_name = signature.first_name
+member.last_name = signature.last_name
+member.email = email unless member.email
+member.save
+
+      signature.member = member
     end
 
     signature.http_referer = retrieve_http_referer
     ref_code = Referral.where(code: params[:signer_ref_code]).first || Referral.new(code: params[:signer_ref_code])
-    if signature.valid?
-      begin
-        signature.track_referrals(params)
-        signature.save!
-        ref_code.member_id = signature.member.id
-        ref_code.petition_id = petition.id
-        ref_code.save!
 
-        SignedPetitionEmailJob.perform_async(signature.id)
+    Signature.transaction do
+      if signature.valid?
+        begin
+          signature.track_referrals(params)
+          signature.save!
+          member.save!
+          ref_code.member_id = signature.member.id
+          ref_code.petition_id = petition.id
+          ref_code.save!
 
-        nps_win signature
-        win! :signature
-        cookies[:member_id] = { :value => signature.member.to_hash, :expires => 100.years.from_now }
-        cookies[:ref_code] = { :value => ref_code.code, :expires => 100.years.from_now }
+          SignedPetitionEmailJob.perform_async(signature.id)
 
-        flash[:signature_id] = signature.id
-      rescue => ex
-        notify_airbrake(ex)
-        Rails.logger.error "Error saving signature: #{ex} #{ex.backtrace.join}"
-        flash.notice = ex.message
+          nps_win signature
+          win! :signature
+          cookies[:member_id] = { :value => signature.member.to_hash, :expires => 100.years.from_now }
+          cookies[:ref_code] = { :value => ref_code.code, :expires => 100.years.from_now }
+
+          flash[:signature_id] = signature.id
+        rescue => ex
+          notify_airbrake(ex)
+          Rails.logger.error "Error saving signature: #{ex} #{ex.backtrace.join("\n")}"
+          flash.notice = ex.message
+        end
       end
     end
 
