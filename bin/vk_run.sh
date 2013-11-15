4ef8a76e727a92c98a0ee2830bfd5fe530cccd7b
Mark Steckel <mjs@fix.net>
2013-11-15 09:18:16 -0800

    Tweaks for running things via cron. (The PATH needs to know about rbenv, and minimize outout so cron does not send email for normal execution.)

--- bin/vk_run.sh
+++ bin/vk_run.sh
@@ -7,9 +7,13 @@ appdir="${topdir}/${application}"
 current_path="${appdir}/current"
 shared_path="${appdir}/shared"
 
+if [ -z "$RBENV_SHELL" ] ; then
+  PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH:/usr/local/sbin:/usr/local/bin:$HOME/bin"; export PATH
+  eval "$(rbenv init -)"
+fi
+
 . "/home/admin/vk/current/bin/vk_env.sh"
 
 cd "${appdir}/current"
 
-echo bundle exec $@
 exec bundle exec $@
