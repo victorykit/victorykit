c88f82750559134cd26b08e9d9b707df1845d7c4
Mark Steckel <mjs@fix.net>
2014-01-03 14:54:45 -0800

    Created Sidekiq worker to geolocate a signature so it can be performed out of band (async).

---
+++ app/workers/geo_locate_signature_job.rb
@@ -0,0 +1,25 @@
+class GeoLocateSignatureJob
+  include Sidekiq::Worker
+
+  def perform(signature_id)
+    Signature.transaction do 
+      sig = Signature.find_by_id(signature_id)
+
+      return if sig.nil?
+
+      loc = Geocoder.search(sig.ip_address).first 
+
+      sig.city         = loc.city         if loc.city.present?
+      sig.metrocode    = loc.metrocode    if loc.metrocode.present?
+      sig.state        = loc.state        if loc.state.present?
+      sig.state_code   = loc.state_code   if loc.state_code.present?
+      sig.country_code = loc.country_code if loc.country_code.present?
+      
+      sig.member.state_code   = loc.state_code   if loc.state_code.present?
+      sig.member.country_code = loc.country_code if loc.country_code.present?
+
+      sig.save!
+    end
+  end
+
+end
